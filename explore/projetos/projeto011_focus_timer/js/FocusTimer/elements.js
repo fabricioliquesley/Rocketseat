0f5806b48fe65ccb64c14aff0b317b6e02dc4f17
Fabricio <fabricioliquesley15@gmail.com>
2023-11-16 19:22:14 -0300

    feat: create the function of updateDisplay

--- explore/projetos/projeto011_focus_timer/js/FocusTimer/elements.js
+++ explore/projetos/projeto011_focus_timer/js/FocusTimer/elements.js
@@ -1,1 +1,5 @@
 export const controls = document.querySelector('.controls')
+
+export const minutes = document.getElementById('minutes');
+
+export const seconds = document.getElementById('seconds');
--- explore/projetos/projeto011_focus_timer/js/FocusTimer/index.js
+++ explore/projetos/projeto011_focus_timer/js/FocusTimer/index.js
@@ -1,9 +1,12 @@
 import state from "./state.js";
 import * as events from "./events.js";
+import * as timer from "./timer.js";
 
 export function start(minutos, segundos) {
     state.minutes = minutos;
     state.seconds = segundos;
+
+    timer.updateDisplay(4, 4)
 
     events.registerControls();
 }
