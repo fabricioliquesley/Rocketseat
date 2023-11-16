b386028b11b222e8fddfe2e01ef9f077ab4e9923
Fabricio <fabricioliquesley15@gmail.com>
2023-11-16 18:40:11 -0300

    feat: register click events

--- explore/projetos/projeto011_focus_timer/js/FocusTimer/index.js
+++ explore/projetos/projeto011_focus_timer/js/FocusTimer/index.js
@@ -1,7 +1,10 @@
 import state from "./state.js";
+import * as events from "./events.js";
 
 export function start(minutos, segundos) {
     state.minutes = minutos;
     state.seconds = segundos;
+
+    events.registerControls();
 }
 
