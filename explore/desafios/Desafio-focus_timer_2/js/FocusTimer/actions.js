93574901c5d6daad557652dcdcee64c9e3b44c02
Fabricio <fabricioliquesley15@gmail.com>
2023-11-30 20:49:24 -0300

    feat: add actions.js

---
+++ explore/desafios/Desafio-focus_timer_2/js/FocusTimer/actions.js
@@ -0,0 +1,15 @@
+export function toggleRunning () {
+    console.log('toggleRunning');
+}
+
+export function reset () {
+    console.log('reset');
+}
+
+export function incrementFive () {
+    console.log('incrementFive');
+}
+
+export function decrementFive () {
+    console.log('decrementFive');
+}
