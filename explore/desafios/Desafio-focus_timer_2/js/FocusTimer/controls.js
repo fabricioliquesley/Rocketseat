79093bb9e05d652cf8dee9dc3e5dae017b3a60b9
Fabricio <fabricioliquesley15@gmail.com>
2023-11-30 20:34:58 -0300

    fix: rename controls.js to elements.js

--- explore/desafios/Desafio-focus_timer_2/js/FocusTimer/controls.js
+++
@@ -1,5 +0,0 @@
-export const controls = document.querySelector('.timerControls');
-
-export const minutes = document.getElementById('minutes');
-
-export const seconds = document.getElementById('seconds');
