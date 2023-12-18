c099ce11919ce88c506acba23b4aef4a0a7aa05c
Fabricio <fabricioliquesley15@gmail.com>
2023-12-18 20:40:01 -0300

    feat: created the querybuilder to create the table of tags and links

--- explore/node/API-01/knexfile.js
+++ explore/node/API-01/knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    pool: {
+      afterCreate: (connection, callBack) => connection.run("PRAGMA foreign_keys = ON", callBack) 
+    },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
