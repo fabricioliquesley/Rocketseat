8bb54ab80a140dced28564a62f44bd90f55b4ca2
Fabricio <fabricioliquesley15@gmail.com>
2023-12-22 19:39:15 -0300

    feat: created the connection to the database

--- explore/desafios/Desafio015-API-Filmes/src/server.js
+++ explore/desafios/Desafio015-API-Filmes/src/server.js
@@ -1,13 +1,16 @@
 require("express-async-errors");
 
-const AppError = require("./utils/AppError")
+const AppError = require("./utils/AppError");
+const database = require("./database/sqlite");
 const express = require("express");
-const routes = require("./routes")
+const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
