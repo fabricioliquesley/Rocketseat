e5b71790890515c4350671893b7e55f4cffeccb4
Fabricio <fabricioliquesley15@gmail.com>
2023-12-22 23:24:03 -0300

    feat: method created in userController to update a registered user

--- explore/desafios/Desafio015-API-Filmes/src/controllers/UserController.js
+++ explore/desafios/Desafio015-API-Filmes/src/controllers/UserController.js
@@ -1,6 +1,6 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 class UserController {
     async create(request, response) {
@@ -19,6 +19,51 @@ class UserController {
             email,
             password: hashedPassword
         })
+
+        response.status(201).json();
+    }
+
+    async update(request, response) {
+        const { name, email, password, oldPassword } = request.body;
+        const { id } = request.params;
+
+        const user = await knex("users").select().where("id", id);
+
+        if (user.length == 0) {
+            throw new AppError("Usuário não encontrado");
+        }
+
+        if (email){
+            const userWithUpdatedEmail = await knex("users").select().where("email", email);
+
+            if (userWithUpdatedEmail.length > 0 && userWithUpdatedEmail[0].id !== user[0].id) {
+                throw new AppError("Este e-mail já está em uso.");
+            }
+        }
+
+        user[0].name = name ?? user[0].name;
+        user[0].email = email ?? user[0].email;
+
+        console.log([user[0].name, user[0].email])
+
+        if (password && oldPassword) {
+            const checkPassword = await compare(oldPassword, user[0].password);
+
+            if (!checkPassword) {
+                throw new AppError("A senha antiga não é igual a cadastrada.");
+            }
+
+            user[0].password = await hash(password, 8);
+        }
+
+        console.log([user[0].password])
+
+        await knex("users").update({
+            name: user[0].name,
+            email: user[0].email,
+            password: user[0].password,
+            updated_at: knex.fn.now()
+        }).where("id", id)
 
         response.status(201).json();
     }
--- explore/desafios/Desafio015-API-Filmes/src/routes/users.routes.js
+++ explore/desafios/Desafio015-API-Filmes/src/routes/users.routes.js
@@ -7,5 +7,6 @@ const userController = new UserController();
 const usersRoutes = Router();
 
 usersRoutes.post("/", userController.create);
+usersRoutes.put("/:id", userController.update);
 
 module.exports = usersRoutes;
