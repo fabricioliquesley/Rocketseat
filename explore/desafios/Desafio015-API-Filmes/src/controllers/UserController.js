22c455c552e5af55dccf60a2876059d327c3d7a5
Fabricio <fabricioliquesley15@gmail.com>
2023-12-22 21:48:57 -0300

    feat: method created to register new users in the userController

--- explore/desafios/Desafio015-API-Filmes/src/controllers/UserController.js
+++ explore/desafios/Desafio015-API-Filmes/src/controllers/UserController.js
@@ -1,8 +1,26 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { hash } = require("bcryptjs");
+
 class UserController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
-        response.json({name, email, password});
+        const checkUserExists = await knex("users").select().where("email", email)
+
+        if (checkUserExists.length > 0) {
+            throw new AppError("Este e-mail já está cadastrado.")
+        }
+
+        const hashedPassword = await hash(password, 8);
+
+        await knex("users").insert({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        response.status(201).json();
     }
 }
 
