bf27ced299a5f5cd1a73800fe6a8761cdeb8ba8f
Fabricio <fabricioliquesley15@gmail.com>
2023-12-25 22:21:45 -0300

    feat: added the header component in detail page

--- explore/projetos/projeto014-React-Rocketnotes/src/pages/Details/index.jsx
+++ explore/projetos/projeto014-React-Rocketnotes/src/pages/Details/index.jsx
@@ -1,9 +1,14 @@
 import { Container } from "./style.js";
+import { Header } from "../../components/Header/index.jsx";
 import { Button } from "../../components/Button/index.jsx";
 
 export function Details(){
     return (
         <Container>
+            <Header 
+                name="Fabricio Liquesley"
+                src="https://github.com/diego3g.png"
+            />
             <Button title="Voltar"/>
         </Container>
     );
--- explore/projetos/projeto014-React-Rocketnotes/src/pages/Details/style.js
+++ explore/projetos/projeto014-React-Rocketnotes/src/pages/Details/style.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    
+    display: grid;
+    grid-template-rows: max-content auto;
+    grid-template-areas:
+    "header"
+    "content";
+    width: 100%;
+    height: 100vh;
 `;
