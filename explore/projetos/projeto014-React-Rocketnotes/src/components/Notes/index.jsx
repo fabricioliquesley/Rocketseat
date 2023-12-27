92fd19c7fbe7397173b03b2ff004762f5748ac69
Fabricio <fabricioliquesley15@gmail.com>
2023-12-26 21:36:08 -0300

    feat: added function to render ul in the notes component

--- explore/projetos/projeto014-React-Rocketnotes/src/components/Notes/index.jsx
+++ explore/projetos/projeto014-React-Rocketnotes/src/components/Notes/index.jsx
@@ -2,17 +2,20 @@ import { Container } from "./style";
 import { Tag } from "../Tag";
 
 export function Notes({ data, ...rest }) {
-    return (
-        <Container {...rest}>
-            <h2>{data.name}</h2>
-            {
-                data.tags &&
+    function renderUl(data){
+        return (
                 <ul>
                     {
                         data.tags.map(tag => <Tag key={tag.id} title={tag.name} />)
                     }
                 </ul>
-            }
+        )
+    }
+
+    return (
+        <Container {...rest}>
+            <h2>{data.name}</h2>
+            {data.tags && renderUl(data)}
         </Container>
     );
 }
