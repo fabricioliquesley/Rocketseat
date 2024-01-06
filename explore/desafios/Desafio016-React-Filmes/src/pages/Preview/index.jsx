4babdb6e3fd60409d64273cbc2311049a3b5713c
Fabricio <fabricioliquesley15@gmail.com>
2024-01-06 16:23:06 -0300

    feat: created a function to delete a note

--- explore/desafios/Desafio016-React-Filmes/src/pages/Preview/index.jsx
+++ explore/desafios/Desafio016-React-Filmes/src/pages/Preview/index.jsx
@@ -5,7 +5,7 @@ import { RatingStar } from "../../components/Rating";
 import { FiClock } from "react-icons/fi";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { Tag } from "../../components/Tag";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
@@ -16,6 +16,26 @@ export function Preview() {
     const [data, setData] = useState({});
 
     const params = useParams();
+    const navigate = useNavigate();
+
+    async function handleDeleteNote(){
+        const confirm = window.confirm("Deseja mesmo excluir essa nota?");
+
+        if(confirm){
+            await api.delete(`/movies?user_id=${user.id}&movie_note_id=${params.id}`)
+                .then(() => {
+                    alert("Nota deletada!");
+                    navigate(-1);
+                })
+                .catch((erro) => {
+                    if(erro.response){
+                        alert(erro.response.data.message);
+                    } else {
+                        alert("Não foi possível excluir a nota!")
+                    }
+                })
+        }
+    }
 
     useEffect(() => {
         async function fetchNotes() {
@@ -36,7 +56,7 @@ export function Preview() {
                 <div>
                     <Controls>
                         <Link title="Voltar" />
-                        <button>
+                        <button onClick={handleDeleteNote}>
                             <FaRegTrashAlt />
                         </button>
                     </Controls>
