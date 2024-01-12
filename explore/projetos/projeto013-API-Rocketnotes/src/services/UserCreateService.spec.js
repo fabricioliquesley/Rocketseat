c75a3d3a259d3376cebbe35a96ef3ada6a56a7d3
Fabricio <fabricioliquesley15@gmail.com>
2024-01-12 11:43:15 -0300

    feat: test created to check that users with similar e-mail addresses are not created

--- explore/projetos/projeto013-API-Rocketnotes/src/services/UserCreateService.spec.js
+++ explore/projetos/projeto013-API-Rocketnotes/src/services/UserCreateService.spec.js
@@ -1,5 +1,6 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
 
 describe("UserCreateService", () => {
     it("user should be create", async () => {
@@ -16,7 +17,23 @@ describe("UserCreateService", () => {
         expect(userCreated).toHaveProperty("id");
     });
 
-    it("", () => {
+    it("user not should be create with exists email", async () => {
+        const user1 = {
+            name: "User Test 1",
+            email: "user@example.com",
+            password: "123"
+        }
 
+        const user2 = {
+            name: "User Test 2",
+            email: "user@example.com",
+            password: "123"
+        }
+
+        const userRepository = new UserRepositoryInMemory();
+        const userCreateService = new UserCreateService(userRepository);
+
+        await userCreateService.execute(user1);
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está cadastrado."));
     });
 })
