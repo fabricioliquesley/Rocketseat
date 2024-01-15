b60c053c54764aabe6359cb5774beae878a65bf2
Fabricio <fabricioliquesley15@gmail.com>
2024-01-15 11:19:29 -0300

    feat: provided routes according to user profile

--- explore/projetos/projeto015-explorer-stock/web/src/routes/index.jsx
+++ explore/projetos/projeto015-explorer-stock/web/src/routes/index.jsx
@@ -4,13 +4,29 @@ import { useAuth } from "../hooks/auth";
 
 import { AdminRoutes } from './admin.routes';
 import { AuthRoutes } from './auth.routes';
+import { CustomerRoutes } from './customer.routes';
+import { SaleRoutes } from './sale.routes';
+import { USER_ROLE } from '../utils/roles';
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.SALE:
+        return <SaleRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+  }
+
   return (
     <BrowserRouter>
-      {user ? <AdminRoutes /> : <AuthRoutes />}
+      {user ? AccessRoute() : <AuthRoutes />}
     </BrowserRouter>
   );
 }
--- explore/projetos/projeto015-explorer-stock/web/src/routes/sale.routes.jsx
+++ explore/projetos/projeto015-explorer-stock/web/src/routes/sale.routes.jsx
@@ -5,13 +5,12 @@ import { Product } from '../pages/Product';
 import { NotFound } from '../pages/NotFound';
 import { SalesReport } from '../pages/SalesReport';
 
-export function AdminRoutes() {
+export function SaleRoutes() {
   return (
     <Routes>
       <Route path="/" element={< Home />} />
       <Route path="/product" element={<Product />} />
       <Route path="/sales-report" element={<SalesReport />} />
-      
       <Route path="*" exact={true} element={<NotFound />} />
     </Routes>
   );
