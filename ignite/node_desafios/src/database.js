56415f173c47467e75e0fffac8558028ac16a5a2
Fabricio <fabricioliquesley15@gmail.com>
2024-04-04 13:52:24 -0300

    feat: create routes to get, update, patch and delete a task

--- ignite/node_desafios/src/database.js
+++ ignite/node_desafios/src/database.js
@@ -29,4 +29,81 @@ export class Database {
         this.#persist();
         return;
     }
+
+    select({ table, search }) {
+        let data = this.#database[table] ?? [];
+
+        const { title, description } = search
+
+        if (title || description) {
+            data = data.filter(row => {
+                return Object.entries(search).some(([key, value]) => {
+                    value = String(value).toLowerCase();
+                    return row[key].toLowerCase().includes(value);
+                })
+            })
+        }
+
+        return data;
+    }
+
+    update({ table, data }) {
+        const { taskId, title, description } = data;
+
+        const taskIndex = this.#database[table].findIndex(row => row.id == taskId);
+
+        if (taskIndex === -1) {
+            return 1;
+        }
+
+        let task = this.#database[table][taskIndex];
+
+        this.#database[table][taskIndex] = {
+            id: taskId,
+            title: title ?? task.name,
+            description: description ?? task.description,
+            completed_at: task.completed_at,
+            created_at: task.created_at,
+            updated_at: new Date().toUTCString()
+        }
+
+        this.#persist();
+
+        return 0;
+    }
+
+    partialUpdate({ table, data }) {
+        const taskIndex = this.#database[table].findIndex(row => row.id == data.taskId);
+
+        if (taskIndex === -1) {
+            return 1;
+        }
+
+        const task = this.#database[table][taskIndex];
+
+        if (task.completed_at != null) {
+            this.#database[table][taskIndex].completed_at = null;
+        }
+        else {
+            this.#database[table][taskIndex].completed_at = new Date().toUTCString();
+        }
+
+        this.#persist();
+
+        return 0;
+    }
+
+    delete({ table, data }) {
+        const taskIndex = this.#database[table].findIndex(row => row.id == data.taskId);
+
+        if (taskIndex === -1) {
+            return 1;
+        }
+        
+        this.#database[table].splice(taskIndex, 1);
+
+        this.#persist();
+
+        return 0;
+    }
 }
--- ignite/node_desafios/src/routes.js
+++ ignite/node_desafios/src/routes.js
@@ -7,7 +7,7 @@ const database = new Database();
 export const routes = [
     {
         method: "POST",
-        path: buildRoutePath("/task"),
+        path: buildRoutePath("/tasks"),
         handler: (request, response) => {
             const { title, description } = request.body;
 
@@ -21,12 +21,88 @@ export const routes = [
                     title,
                     description,
                     completed_at: null,
-                    created_at: new Date().toISOString(),
-                    updated_at: new Date().toISOString(),
+                    created_at: new Date().toUTCString(),
+                    updated_at: new Date().toUTCString(),
                 }
             })
 
             return response.writeHead(200).end()
+        }
+    },
+    {
+        method: "GET",
+        path: buildRoutePath("/tasks"),
+        handler: (request, response) => {
+            const { title, description } = request.query;
+
+            const tasks = database.select({
+                table: "tasks", search: {
+                    title,
+                    description
+                }
+            });
+
+            return response.writeHead(200).end(JSON.stringify(tasks));
+        }
+    },
+    {
+        method: "PUT",
+        path: buildRoutePath("/tasks/:taskId"),
+        handler: (request, response) => {
+            const { taskId } = request.params;
+            const { title, description } = request.body;
+
+            if (!title || !description) {
+                return response.writeHead(404).end(JSON.stringify({ message: "insert a title or description to update the task" }));
+            }
+
+            const updateStatus = database.update({
+                table: "tasks", data: {
+                    taskId,
+                    title,
+                    description
+                }
+            })
+
+            if (updateStatus != 0) {
+                return response.writeHead(404).end(JSON.stringify({ message: "The taskId not register in database" }));
+            }
+
+            return response.writeHead(200).end();
+        }
+    },
+    {
+        method: "PATCH",
+        path: buildRoutePath("/tasks/:taskId/complete"),
+        handler: (request, response) => {
+            const { taskId } = request.params;
+
+            const partialUpdateStatus = database.partialUpdate({
+                table: "tasks", data: {
+                    taskId
+                }
+            })
+
+            if (partialUpdateStatus != 0) {
+                return response.writeHead(404).end(JSON.stringify({ message: "The taskId not register in database" }));
+            }
+
+            return response.writeHead(200).end();
+        }
+    },
+    {
+        method: "DELETE",
+        path: buildRoutePath("/tasks/:taskId"),
+        handler: (request, response) => {
+            const { taskId } = request.params;
+
+            const deleteStatus = database.delete({ table: "tasks", data: { taskId } })
+
+            if (deleteStatus != 0) {
+                return response.writeHead(404).end(JSON.stringify({ message: "The taskId not register in database" }));
+            }
+
+            return response.writeHead(200).end();
         }
     }
 ]
