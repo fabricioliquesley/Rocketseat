33c430fca012781071851e656c99e6b46cfee5a2
Fabricio <fabricioliquesley15@gmail.com>
2024-04-09 13:37:36 -0300

    feat: use cookies in the API and record transaction routes

--- ignite/node/02-api-rest-nodejs/src/server.ts
+++ ignite/node/02-api-rest-nodejs/src/server.ts
@@ -1,20 +1,14 @@
 import fastify from "fastify";
-import { randomUUID } from "node:crypto";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  const transactions = await knex("transactions")
-    .insert({
-      id: randomUUID(),
-      title: "Transação de teste",
-      amount: 1000,
-    })
-    .returning("*");
+app.register(cookie);
 
-  return transactions;
+app.register(transactionsRoutes, {
+  prefix: "transactions",
 });
 
 app
