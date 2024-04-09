85dc5e0e52cee1cd22d1475e7d4724b76b990bc2
Fabricio <fabricioliquesley15@gmail.com>
2024-04-08 21:18:25 -0300

    feat: test the database connection with an insert query

--- ignite/node/02-api-rest-nodejs/src/server.ts
+++ ignite/node/02-api-rest-nodejs/src/server.ts
@@ -1,11 +1,24 @@
 import fastify from "fastify";
+import { randomUUID } from "node:crypto";
+import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
-app.get("/hello", () => "Hello dev");
+app.get("/hello", async () => {
+  const transactions = await knex("transactions")
+    .insert({
+      id: randomUUID(),
+      title: "Transação de teste",
+      amount: 1000,
+    })
+    .returning("*");
+
+  return transactions;
+});
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log("http server running"));
