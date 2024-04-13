2f4e0dc9a6030108032589fdf982140b0e2d45ae
Fabricio <fabricioliquesley15@gmail.com>
2024-04-13 20:24:55 -0300

    feat: create route to create a user session

--- ignite/node_desafios/02-api-restfull-nodejs/src/routes/user-routes.ts
+++ ignite/node_desafios/02-api-restfull-nodejs/src/routes/user-routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { UserRepository } from "../repositories/userRepository";
 import { UserServices } from "../services/usersServices";
+import { throwParametersError } from "../utils/throwParametersError";
 
 export async function userRoutes(app: FastifyInstance) {
   const usersServices = new UserServices(new UserRepository());
 
-  app.post("/users", async (request, reply) => {
+  app.post("/", async (request, reply) => {
     const bodySchema = z.object({
       name: z.string(),
       email: z.string(),
@@ -16,21 +17,56 @@ export async function userRoutes(app: FastifyInstance) {
     const result = bodySchema.safeParse(request.body);
 
     if (!result.success) {
-      const errors = new Array<object>();
-
-      result.error.issues.map((error) => {
-        const { message, path } = error;
-
-        errors.push({ message: `${message} em ${path}` });
-      });
-
-      return reply.status(404).send({ errors });
+      return throwParametersError(result.error.issues, reply);
     }
 
     const { name, email, password } = result.data;
 
-    await usersServices.createUser({data: {name, email, password}});
+    await usersServices.createUser({ data: { name, email, password } });
 
     return reply.status(201).send();
+  });
+
+  app.post("/session", async (request, reply) => {
+    const bodySchema = z.object({
+      email: z.string(),
+      password: z.string(),
+    });
+
+    const userSessionSchema = z.object({
+      id: z.string(),
+      name: z.string(),
+      email: z.string(),
+      created_at: z.string(),
+    });
+
+    const result = bodySchema.safeParse(request.body);
+
+    if (!result.success) {
+      return throwParametersError(result.error.issues, reply);
+    }
+
+    const { email, password } = result.data;
+
+    const user = userSessionSchema.parse(
+      await usersServices.createSession({
+        data: { email, password },
+      })
+    );
+
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      const $7_DAYS = 60 * 60 * 24 * 7;
+
+      sessionId = user.id;
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: $7_DAYS,
+      });
+    }
+
+    return reply.status(200).send({ user });
   });
 }
