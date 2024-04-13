205c839e83a2260a6f920c93921fec6f1927427d
Fabricio <fabricioliquesley15@gmail.com>
2024-04-13 19:52:44 -0300

    feat: register cookies in server

--- ignite/node_desafios/02-api-restfull-nodejs/src/app.ts
+++ ignite/node_desafios/02-api-restfull-nodejs/src/app.ts
@@ -1,6 +1,10 @@
 import fastify from "fastify";
 import { userRoutes } from "./routes/user-routes";
+import cookie from "@fastify/cookie";
 
 export const app = fastify();
 
-app.register(userRoutes)
+app.register(cookie);
+app.register(userRoutes, {
+  prefix: "users",
+});
