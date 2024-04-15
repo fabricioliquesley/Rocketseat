9c9ccdb365364023af11b7915d09169d4536791f
Fabricio <fabricioliquesley15@gmail.com>
2024-04-15 18:06:47 -0300

    feat: register a meal routes in server

--- ignite/node_desafios/02-api-restfull-nodejs/src/app.ts
+++ ignite/node_desafios/02-api-restfull-nodejs/src/app.ts
@@ -1,10 +1,16 @@
 import fastify from "fastify";
 import { userRoutes } from "./routes/user-routes";
 import cookie from "@fastify/cookie";
+import { mealRoutes } from "./routes/meal-routes";
 
 export const app = fastify();
 
 app.register(cookie);
+
 app.register(userRoutes, {
   prefix: "users",
 });
+
+app.register(mealRoutes, {
+  prefix: "meals"
+})
