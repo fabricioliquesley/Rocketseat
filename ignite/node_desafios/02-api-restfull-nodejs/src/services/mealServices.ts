f4a4ed7eeddb46e703d5a1f83366c20373c8b2c9
Fabricio <fabricioliquesley15@gmail.com>
2024-04-16 14:05:59 -0300

    feat: create a logic to handle the editing of a meal

--- ignite/node_desafios/02-api-restfull-nodejs/src/services/mealServices.ts
+++ ignite/node_desafios/02-api-restfull-nodejs/src/services/mealServices.ts
@@ -1,22 +1,34 @@
 import { randomUUID } from "crypto";
 import { RepositorySchema } from "../repositories/mealRepository";
-import { z } from "zod";
 
 interface Meal {
   createMeal({}: parametersSchema): Promise<string>;
   getAllMeals({}: parametersSchemaNoData): Promise<object>;
   getMeal({}: parametersSchemaNoData): Promise<object>;
   deleteMeal({}: parametersSchemaNoData): Promise<number>;
+  editMeal({}: parametersSchema): Promise<editResponse>;
 }
+
+type editResponse =
+  | {
+      id: string;
+      name: string;
+      description: string;
+      diet_compliant: string;
+      user_id: string;
+      created_at: string;
+    }
+  | number;
 
 type parametersSchema = {
   table?: string;
   data: {
     name: string;
     description: string;
-    user_id: string;
+    user_id?: string;
     diet_compliant: string;
   };
+  mealId?: string;
 };
 
 type parametersSchemaNoData = {
@@ -68,25 +80,86 @@ export class MealServices implements Meal {
     return userMeal;
   }
 
-  async deleteMeal({ mealId, sessionId }: parametersSchemaNoData): Promise<number> {
+  async deleteMeal({
+    mealId,
+    sessionId,
+  }: parametersSchemaNoData): Promise<number> {
     type mealSchema = {
-      user_id: string
-    }
+      user_id: string;
+    };
 
     type userSchema = {
-      id: string
-    }
+      id: string;
+    };
 
-    const user: userSchema = await this.#mealRepository.getUser({table: "users", sessionId});
+    const user: userSchema = await this.#mealRepository.getUser({
+      table: "users",
+      sessionId,
+    });
 
-    const meal: mealSchema = await this.#mealRepository.getMeal({table: "meals", mealId});
+    const meal: mealSchema = await this.#mealRepository.getMeal({
+      table: "meals",
+      mealId,
+    });
 
     if (user?.id != meal?.user_id) {
       return 2;
-    }    
+    }
 
     const result = this.#mealRepository.deleteMeal({ table: "meals", mealId });
 
     return result;
+  }
+
+  async editMeal({ data, mealId }: parametersSchema): Promise<editResponse> {
+    type mealSchema = {
+      id?: string;
+      name?: string;
+      description?: string;
+      diet_compliant?: string;
+      user_id: string;
+      created_at?: string;
+    };
+
+    type userSchema = {
+      id: string;
+    };
+
+    const { name, description, user_id, diet_compliant } = data;
+
+    const user: userSchema = await this.#mealRepository.getUser({
+      table: "users",
+      sessionId: user_id,
+    });
+
+    const meal: mealSchema = await this.#mealRepository.getMeal({
+      table: "meals",
+      mealId,
+    });
+
+    if (user?.id != meal?.user_id) {
+      return 2;
+    }
+
+    await this.#mealRepository.editMeal({
+      table: "meals",
+      mealId,
+      data: {
+        name,
+        description,
+        diet_compliant,
+      },
+    });
+
+    const editMeal = {
+      id: String(meal.id),
+      name,
+      description,
+      diet_compliant,
+      user_id: String(meal.user_id),
+      created_at: String(meal.created_at)
+    };
+
+    return editMeal;
   }
 }
