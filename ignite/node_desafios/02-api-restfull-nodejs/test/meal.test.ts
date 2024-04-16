d75e815090951178c4a800f2bc2ee29313da7846
Fabricio <fabricioliquesley15@gmail.com>
2024-04-15 21:03:34 -0300

    feat: create a test to check whether it is possible to obtain all the user's meals and a test to obtain a specific meal

--- ignite/node_desafios/02-api-restfull-nodejs/test/meal.test.ts
+++ ignite/node_desafios/02-api-restfull-nodejs/test/meal.test.ts
@@ -3,6 +3,12 @@ import request from "supertest";
 import { it, describe, beforeEach, beforeAll, afterAll, expect } from "vitest";
 import { app } from "../src/app";
 
+type mealSchema = {
+  name: string;
+  description: string;
+  diet_compliant: string;
+};
+
 beforeAll(async () => {
   await app.ready();
 });
@@ -12,8 +18,8 @@ afterAll(async () => {
 });
 
 beforeEach(() => {
-  execSync("npm run knex migrate:rollback --all");
-  execSync("npm run knex migrate:latest");
+  execSync("npm run knex -- migrate:rollback --all");
+  execSync("npm run knex -- migrate:latest");
 });
 
 describe("Meal routes", () => {
@@ -29,5 +35,75 @@ describe("Meal routes", () => {
         mealId: expect.stringContaining("-"),
       })
     );
+  });
+
+  it("should be able to list all meals", async () => {
+    await request(app.server).post("/users").send({
+      name: "New user",
+      email: "user@example.com",
+      password: "secretPassword",
+    });
+
+    const createSessionResponse = await request(app.server)
+      .post("/users/session")
+      .send({
+        email: "user@example.com",
+        password: "secretPassword",
+      });
+
+    const cookie = createSessionResponse.get("Set-Cookie");
+
+    await request(app.server).post("/meals").send({
+      name: "Frango grelhado",
+      description: "Fatias de peito de frango grelhada, com batata doce!",
+      diet_compliant: "yes",
+    });
+
+    const mealsList = await request(app.server)
+      .get("/meals")
+      .set("Cookie", String(cookie));
+
+    expect(mealsList.body).toHaveProperty("meals");
+    expect(Array.isArray(mealsList.body.meals)).toBe(true);
+    mealsList.body.meals.forEach((meal: mealSchema) => {
+      expect(meal).toEqual(
+        expect.objectContaining({
+          name: "Frango grelhado",
+          description: "Fatias de peito de frango grelhada, com batata doce!",
+          diet_compliant: "yes",
+        })
+      );
+    });
+  });
+
+  it("should be able to get a specific meal", async () => {
+    await request(app.server).post("/users").send({
+      name: "New user",
+      email: "user@example.com",
+      password: "secretPassword",
+    });
+
+    const createSessionResponse = await request(app.server)
+      .post("/users/session")
+      .send({
+        email: "user@example.com",
+        password: "secretPassword",
+      });
+
+    const cookie = createSessionResponse.get("Set-Cookie");
+
+    const mealIdResponse = await request(app.server).post("/meals").send({
+      name: "Frango grelhado",
+      description: "Fatias de peito de frango grelhada, com batata doce!",
+      diet_compliant: "yes",
+    });
+
+    const mealId = mealIdResponse.body.mealId;
+
+    const meal = await request(app.server)
+      .get(`/meals/${mealId}`)
+      .set("Cookie", String(cookie));
+
+    expect(meal.body.meal.id).toEqual(mealId);
   });
 });
