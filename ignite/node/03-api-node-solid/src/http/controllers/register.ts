e6375cf7b39425e6ebce64fef62ef22a7fa401ca
Fabricio <fabricioliquesley15@gmail.com>
2024-04-23 11:29:00 -0300

    feat: create a register controller

---
+++ ignite/node/03-api-node-solid/src/http/controllers/register.ts
@@ -0,0 +1,30 @@
+import { PrismaUserRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+import { RegisterUseCase } from "@/use-cases/register";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUserRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+
+    await registerUseCase.executeRegister({ name, email, password });
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({message: error.message});
+    }
+
+    return reply.status(500).send();
+  }
+
+  return reply.status(201).send();
+}
