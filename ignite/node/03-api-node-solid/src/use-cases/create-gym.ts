7b097b16e3f0458dcf8aef8b4b358bd38bc30c6a
Fabricio <fabricioliquesley15@gmail.com>
2024-04-24 15:19:01 -0300

    feat: create use case create-gym

---
+++ ignite/node/03-api-node-solid/src/use-cases/create-gym.ts
@@ -0,0 +1,38 @@
+import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { Gym } from "@prisma/client";
+import { GymSRepository } from "@/repositories/gyms-repository";
+
+interface CreateGymUseCaseRequest {
+  title: string;
+  description: string | null;
+  phone: string | null;
+  latitude: number;
+  longitude: number;
+}
+
+interface CreateGymUseCaseResponse {
+  gym: Gym;
+}
+
+export class CreateGymUseCase {
+  constructor(private gymsRepository: GymSRepository) {}
+
+  async execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  }: CreateGymUseCaseRequest): Promise<CreateGymUseCaseResponse> {
+    const gym = await this.gymsRepository.createGym({
+      title,
+      description,
+      phone,
+      latitude,
+      longitude,
+    });
+
+    return { gym };
+  }
+}
