b6929ba31823dd43cf2781565d971f0415b906a9
Fabricio <fabricioliquesley15@gmail.com>
2024-04-24 15:14:22 -0300

    feat: creat method findById in inMemoryUsersRepository

--- ignite/node/03-api-node-solid/src/repositories/in-memory/in-memory-users-repository.ts
+++ ignite/node/03-api-node-solid/src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,14 +1,25 @@
 import { Prisma, User } from "@prisma/client";
 import { userRepository } from "../users-repository";
+import { randomUUID } from "node:crypto";
 
 export class InMemoryUserRepository implements userRepository {
   public items: User[] = [];
 
-  async findByEmail(email: string): Promise<User | null> {
-    const user = this.items.find(item => item.email === email);
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((item) => item.id === id);
 
-    if (!user){
-      return null
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find((item) => item.email === email);
+
+    if (!user) {
+      return null;
     }
 
     return user;
@@ -16,12 +27,12 @@ export class InMemoryUserRepository implements userRepository {
 
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
     const user: User = {
-      id: "user-1",
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
-      created_at: new Date()
-    }
+      created_at: new Date(),
+    };
 
     this.items.push(user);
 
