bff403fd2d9a7a68926a4b06e0eafcecb9c54eef
Fabricio <fabricioliquesley15@gmail.com>
2024-04-24 11:04:26 -0300

    feat: refactor the register controller to use a factory

--- ignite/node/03-api-node-solid/src/http/controllers/register.ts
+++ ignite/node/03-api-node-solid/src/http/controllers/register.ts
@@ -1,6 +1,5 @@
-import { PrismaUserRepository } from "@/repositories/prisma/prisma-users-repository";
 import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
-import { RegisterUseCase } from "@/use-cases/register";
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
@@ -14,8 +13,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const prismaUsersRepository = new PrismaUserRepository();
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+    const registerUseCase = makeRegisterUseCase()
 
     await registerUseCase.executeRegister({ name, email, password });
   } catch (error) {
