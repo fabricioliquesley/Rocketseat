562eb9e80509f4a00d9fc13153f51ee2ffb2eae1
Fabricio <fabricioliquesley15@gmail.com>
2024-04-25 16:11:19 -0300

    feat: create use case GetUserMetrics

---
+++ ignite/node/03-api-node-solid/src/use-cases/get-user-metrics.ts
@@ -0,0 +1,22 @@
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "@/repositories/ckeck-ins-repository";
+
+interface GetUserMetricsUseCaseRequest {
+  userId: string;
+}
+
+interface GetUserMetricsUseCaseResponse {
+  checkCount: number;
+}
+
+export class GetUserMetricsUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId
+  }: GetUserMetricsUseCaseRequest): Promise<GetUserMetricsUseCaseResponse> {
+    const checkCount = await this.checkInsRepository.countByUserId(userId);
+
+    return { checkCount };
+  }
+}
