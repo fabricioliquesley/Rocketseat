ddcb557e1997489cc5cebde6f4e2a6818ce19797
Fabricio <fabricioliquesley15@gmail.com>
2024-04-25 16:07:33 -0300

    feat: implements the methods searchMany and findManyNearby in InMemoryCheckInRepository

--- ignite/node/03-api-node-solid/src/repositories/in-memory/in-memory-gyms-repository.ts
+++ ignite/node/03-api-node-solid/src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,7 +1,8 @@
 import { Gym, Prisma } from "@prisma/client";
-import { GymSRepository } from "../gyms-repository";
+import { FindManyNearbyParams, GymSRepository } from "../gyms-repository";
 import { Decimal } from "@prisma/client/runtime/library";
 import { randomUUID } from "node:crypto";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
 export class InMemoryGymSRepository implements GymSRepository {
   public items: Gym[] = [];
@@ -23,11 +24,31 @@ export class InMemoryGymSRepository implements GymSRepository {
       description: data.description ?? null,
       phone: data.phone ?? null,
       latitude: new Prisma.Decimal(data.latitude.toString()),
-      longitude: new Prisma.Decimal(data.longitude.toString())
+      longitude: new Prisma.Decimal(data.longitude.toString()),
     };
 
     this.items.push(gym);
 
-    return gym
+    return gym;
+  }
+
+  async searchMany(query: string, page: number) {
+    return this.items
+      .filter((item) => item.title.includes(query))
+      .slice((page - 1) * 20, page * 20);
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: Number(item.latitude),
+          longitude: Number(item.longitude),
+        }
+      );
+
+      return distance < 10;
+    });
   }
 }
