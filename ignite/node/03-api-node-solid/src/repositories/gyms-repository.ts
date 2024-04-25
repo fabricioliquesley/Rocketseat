30f220f1a90a674a97ac75984b748628a92ff495
Fabricio <fabricioliquesley15@gmail.com>
2024-04-25 16:04:27 -0300

    feat: add methods searchMany and findManyNearby in GymsRepository

--- ignite/node/03-api-node-solid/src/repositories/gyms-repository.ts
+++ ignite/node/03-api-node-solid/src/repositories/gyms-repository.ts
@@ -1,6 +1,13 @@
 import { Gym, Prisma } from "@prisma/client";
 
+export interface FindManyNearbyParams {
+  latitude: number;
+  longitude: number;
+}
+
 export interface GymSRepository {
   findById(gymId: string): Promise<Gym | null>;
   createGym(data: Prisma.GymCreateInput): Promise<Gym>;
+  searchMany(query: string, page: number): Promise<Gym[]>;
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>;
 }
