a95411164bbef8e853a7255d14ddf6af9b4aa9bc
Fabricio <fabricioliquesley15@gmail.com>
2024-04-25 16:12:19 -0300

    feat: create use case SearchGyms

---
+++ ignite/node/03-api-node-solid/src/use-cases/search-gyms.ts
@@ -0,0 +1,24 @@
+import { Gym } from "@prisma/client";
+import { GymSRepository } from "@/repositories/gyms-repository";
+
+interface SearchGymsUseCaseRequest {
+  query: string;
+  page: number;
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[];
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymSRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page);
+
+    return { gyms };
+  }
+}
