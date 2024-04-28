cf01abc5b9b21f6ee52cb73d95e67b4a09efdb2e
Fabricio <fabricioliquesley15@gmail.com>
2024-04-28 13:04:30 -0300

    feat: create sidebar component

--- ignite/react/01-fundamentos-reactjs/src/components/Sidebar/index.jsx
+++ ignite/react/01-fundamentos-reactjs/src/components/Sidebar/index.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "phosphor-react"
 
 import styles from "./Sidebar.module.css"
+import { Avatar } from "../Avatar"
 
 export function Sidebar() {
   return (
@@ -10,16 +11,13 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1478250242432-9381e12b763b?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/diego3g.png"
-        />
+        <Avatar source={"https://github.com/diego3g.png"} />
         <strong>Diego Fernandes</strong>
         <span>Web Developer</span>
       </div>
       <footer>
         <a href="#">
-          <PencilLine size={20}/>
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
