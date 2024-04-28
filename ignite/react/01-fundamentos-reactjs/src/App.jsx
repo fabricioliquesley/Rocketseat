b3c511bf658a647a887340a4d1c52460bf304703
Fabricio <fabricioliquesley15@gmail.com>
2024-04-27 21:34:27 -0300

    feat: add styles in App component

--- ignite/react/01-fundamentos-reactjs/src/App.jsx
+++ ignite/react/01-fundamentos-reactjs/src/App.jsx
@@ -2,19 +2,26 @@ import { Header } from "./components/Header"
 import { Post } from "./components/Post"
 
 import "./global.css"
+import styles from "./App.module.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
-    <main>
+    <div>
       <Header />
-      <Post
-        author="Fabrício Santos"
-        content="JavaScript é incrível ❤"
-      />
-      <Post
-        author={"Gabriel Guedes"}
-        content={"Esculte minha musica nova"}
-      />
-    </main>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Fabrício Santos"
+            content="JavaScript é incrível ❤"
+          />
+          <Post
+            author={"Gabriel Guedes"}
+            content={"Esculte minha musica nova"}
+          />
+        </main>
+      </div>
+    </div>
   )
 }
