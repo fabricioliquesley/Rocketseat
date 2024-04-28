baaf858dfd0165dd580fc302dc580de5dd8b7b09
Fabricio <fabricioliquesley15@gmail.com>
2024-04-28 13:05:50 -0300

    feat: use post component

--- ignite/react/01-fundamentos-reactjs/src/App.jsx
+++ ignite/react/01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header"
 import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
 
 import "./global.css"
 import styles from "./App.module.css"
-import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
@@ -12,14 +12,7 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Fabrício Santos"
-            content="JavaScript é incrível ❤"
-          />
-          <Post
-            author={"Gabriel Guedes"}
-            content={"Esculte minha musica nova"}
-          />
+          <Post />
         </main>
       </div>
     </div>
