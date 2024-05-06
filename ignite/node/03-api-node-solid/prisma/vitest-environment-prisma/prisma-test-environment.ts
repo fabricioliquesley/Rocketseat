6f7c36df7e1c3ffe976cde630d503fe0a062f3d8
Fabricio <fabricioliquesley15@gmail.com>
2024-05-06 10:04:07 -0300

    feat: create an isolated context for each E2E test

--- ignite/node/03-api-node-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ ignite/node/03-api-node-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,41 @@
+import "dotenv/config";
+
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+import { execSync } from "node:child_process";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable.");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   async setup() {
-    console.log("Setup");
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync("npx prisma migrate deploy");
 
     return {
-      teardown() {
-        console.log("Teardown");
-      }
-    }
+      async teardown() {
+        const query = `DROP SCHEMA IF EXISTS "${schema}" CASCADE`;
+
+        await prisma.$executeRawUnsafe(query);
+        await prisma.$disconnect();
+      },
+    };
   },
-  transformMode: "ssr"
+  transformMode: "ssr",
 };
