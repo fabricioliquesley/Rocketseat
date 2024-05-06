70f811e422f8a5fc8e7b34cda0461bc4e832034b
Fabricio <fabricioliquesley15@gmail.com>
2024-05-06 14:00:32 -0300

    chore: move user controllers to user folder

--- ignite/node/03-api-node-solid/src/http/controllers/users/profile.ts
+++ ignite/node/03-api-node-solid/src/http/controllers/users/profile.ts
@@ -1,11 +1,18 @@
 import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  const requestUserSchema = z.object({
+    sub: z.string(),
+  });
+
   const getUserProfile = makeGetUserProfileUseCase();
 
+  const { sub } = requestUserSchema.parse(request.user);
+
   const { user } = await getUserProfile.execute({
-    userId: request.user.sub,
+    userId: sub,
   });
 
   return reply.status(200).send({
