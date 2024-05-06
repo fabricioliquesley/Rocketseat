6c038edf4479b5fc65835e354b5cc8d1599630fa
Fabricio <fabricioliquesley15@gmail.com>
2024-05-06 14:06:00 -0300

    feat: create search controller

---
+++ ignite/node/03-api-node-solid/src/http/controllers/gyms/search.ts
@@ -0,0 +1,21 @@
+import { makeSearchGymsUseCase } from "@/use-cases/factories/make-search-gyms-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function searchGyms(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { page, query } = searchGymQuerySchema.parse(request.query);
+
+  const searchGymsUseCase = makeSearchGymsUseCase();
+
+  const { gyms } = await searchGymsUseCase.execute({
+    query,
+    page,
+  });
+
+  return reply.status(200).send({ gyms });
+}
