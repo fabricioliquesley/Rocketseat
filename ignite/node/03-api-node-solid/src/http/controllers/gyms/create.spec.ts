a1766274c6b5c54a3f1f10b436275e17ab403c17
Fabricio <fabricioliquesley15@gmail.com>
2024-05-06 14:04:18 -0300

    feat: add create gym e2e test

---
+++ ignite/node/03-api-node-solid/src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Create Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("Should be able to create Gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Javascript Gym",
+        description: "Node é incrível",
+        phone: "4002-8922",
+        latitude: -20.3833472,
+        longitude: -43.4163019,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
