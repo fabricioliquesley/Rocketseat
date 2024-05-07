4b452251c39322f032134d7b04258873e04b171f
Fabricio <fabricioliquesley15@gmail.com>
2024-05-07 19:22:25 -0300

    feat: use verifyUserRole middleware in routes

--- ignite/node/03-api-node-solid/src/http/controllers/gyms/routes.ts
+++ ignite/node/03-api-node-solid/src/http/controllers/gyms/routes.ts
@@ -4,11 +4,13 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 import { create } from "./create";
 import { searchGyms } from "./search";
 import { fetchNearbyGyms } from "./nearby";
+import { verifyUserRole } from "@/http/middlewares/verify-user-role";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
-  app.post("/gyms", create);
+  app.post("/gyms", {onRequest: [verifyUserRole("ADMIN")]}, create);
+
   app.get("/gyms/search", searchGyms);
   app.get("/gyms/nearby", fetchNearbyGyms);
 }
