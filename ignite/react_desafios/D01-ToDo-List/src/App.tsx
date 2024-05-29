6b82564acb630729f1ca2416b6091df4b2818fb5
Fabricio <fabricioliquesley15@gmail.com>
2024-05-29 11:20:42 -0300

    feat: attaches the components to the home screen

--- ignite/react_desafios/D01-ToDo-List/src/App.tsx
+++ ignite/react_desafios/D01-ToDo-List/src/App.tsx
@@ -1,11 +1,21 @@
-import "./globals.css"
+import "./globals.css";
+import styles from "./App.module.css";
+import { Logo } from "./components/Logo";
+import { Input } from "./components/Input";
+import { SubmitButton } from "./components/SubmitButton";
+import { DeleteButton } from "./components/DeleteButton";
 
 function App() {
   return (
-    <>
-      <h1>Hello, World</h1>
-    </>
-  )
+    <div className={styles.wrapper}>
+      <Logo />
+      <form className={styles.formWrapper}>
+        <Input placeholder="Adicione uma nova tarefa" />
+        <SubmitButton />
+      </form>
+      <DeleteButton />
+    </div>
+  );
 }
 
-export default App
+export default App;
