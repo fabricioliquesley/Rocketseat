e5aa752e4ea67258636bb9d66d885ad0f5aebcd2
Fabricio <fabricioliquesley15@gmail.com>
2024-05-30 13:31:17 -0300

    feat: add input properties to the checkbox component

--- ignite/react_desafios/D01-ToDo-List/src/components/CheckBox/index.tsx
+++ ignite/react_desafios/D01-ToDo-List/src/components/CheckBox/index.tsx
@@ -1,9 +1,12 @@
+import { InputHTMLAttributes } from "react";
 import styles from "./Checkbox.module.css";
 
-export function CheckBox() {
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function CheckBox({...props}: CheckboxProps) {
   return (
     <div className={styles.check_box_wrapper}>
-      <input type="checkbox" className={styles.check_box} />
+      <input type="checkbox" className={styles.check_box} {...props}/>
       <div className={styles.check_box_label}></div>
     </div>
   );
