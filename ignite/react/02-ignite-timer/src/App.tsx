eaba07981691d912780f6b47fa1e0111db1fbc1d
Fabricio <fabricioliquesley15@gmail.com>
2024-06-03 10:36:47 -0300

    feat: use project theme and global styles

--- ignite/react/02-ignite-timer/src/App.tsx
+++ ignite/react/02-ignite-timer/src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyles } from "./styles/global.styles"
+
 export function App() {
   return (
-    <h1>Hello, world</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello, world</h1>
+      
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
