3f0a7cbb46fab9ffe0c198c3330bb3979dab398c
Fabricio <fabricioliquesley15@gmail.com>
2024-06-03 13:48:09 -0300

    feat: create a basic server with fastify

---
+++ ignite/node_desafios/03-api-find-a-friend/src/server.ts
@@ -0,0 +1,18 @@
+import { app } from "./app";
+import { env } from "@/env";
+
+app.get("/", () => {
+  return "hello world"
+})
+
+const start = async () => {
+  try {
+    await app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
+      console.log(`server is listening on port ${env.PORT}`);
+    })
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+start()
