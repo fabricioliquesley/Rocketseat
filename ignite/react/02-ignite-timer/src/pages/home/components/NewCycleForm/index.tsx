d66f07bd0d2c84613cf6c0879309c4fef436c0b2
Fabricio <fabricioliquesley15@gmail.com>
2024-06-05 15:00:58 -0300

    feat: separate the form in one new component

---
+++ ignite/react/02-ignite-timer/src/pages/home/components/NewCycleForm/index.tsx
@@ -0,0 +1,33 @@
+import { FormContainer, TaskInput, MinutesAmountInput } from "./styles"
+
+export function NewCycleForm() {
+  return (
+    <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list={"task-suggestions"}
+            disabled={!!activeCycle}
+            {...register("task")}
+          />
+          <datalist id="task-suggestions">
+            <option value="gerenciador de senhas" />
+          </datalist>
+          <label htmlFor="minutesAmount">durante</label>
+          <div>
+            <MinutesAmountInput
+              id="minutesAmount"
+              type="number"
+              placeholder="00"
+              step={5}
+              min={1}
+              max={60}
+              disabled={!!activeCycle}
+              {...register("minutesAmount", { valueAsNumber: true })}
+            />
+          </div>
+          <span>minutos.</span>
+        </FormContainer>F
+  )
+}
