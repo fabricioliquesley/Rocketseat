22fa1660f5e2207c07daf0005f536ae959372ff2
Fabricio <fabricioliquesley15@gmail.com>
2024-06-06 09:37:41 -0300

    feat: use the cycle context to implement the logic of the newCycleForm component

--- ignite/react/02-ignite-timer/src/pages/home/components/NewCycleForm/index.tsx
+++ ignite/react/02-ignite-timer/src/pages/home/components/NewCycleForm/index.tsx
@@ -1,33 +1,39 @@
-import { FormContainer, TaskInput, MinutesAmountInput } from "./styles"
+import { FormContainer, TaskInput, MinutesAmountInput } from "./styles";
+import { useContext } from "react";
+import { CyclesContext } from "../..";
+import { useFormContext } from "react-hook-form";
 
 export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
+
   return (
     <FormContainer>
-          <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput
-            id="task"
-            placeholder="Dê um nome para o seu projeto"
-            list={"task-suggestions"}
-            disabled={!!activeCycle}
-            {...register("task")}
-          />
-          <datalist id="task-suggestions">
-            <option value="gerenciador de senhas" />
-          </datalist>
-          <label htmlFor="minutesAmount">durante</label>
-          <div>
-            <MinutesAmountInput
-              id="minutesAmount"
-              type="number"
-              placeholder="00"
-              step={5}
-              min={1}
-              max={60}
-              disabled={!!activeCycle}
-              {...register("minutesAmount", { valueAsNumber: true })}
-            />
-          </div>
-          <span>minutos.</span>
-        </FormContainer>F
-  )
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        placeholder="Dê um nome para o seu projeto"
+        list={"task-suggestions"}
+        disabled={!!activeCycle}
+        {...register("task")}
+      />
+      <datalist id="task-suggestions">
+        <option value="gerenciador de senhas" />
+      </datalist>
+      <label htmlFor="minutesAmount">durante</label>
+      <div>
+        <MinutesAmountInput
+          id="minutesAmount"
+          type="number"
+          placeholder="00"
+          step={5}
+          min={5}
+          max={60}
+          disabled={!!activeCycle}
+          {...register("minutesAmount", { valueAsNumber: true })}
+        />
+      </div>
+      <span>minutos.</span>
+    </FormContainer>
+  );
 }
