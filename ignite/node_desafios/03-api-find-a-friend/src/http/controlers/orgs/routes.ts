0ba9d0581fd6997569b368744d440206df8b97c4
Fabricio <fabricioliquesley15@gmail.com>
2024-06-11 11:24:55 -0300

    feat: create route to refresh token

--- ignite/node_desafios/03-api-find-a-friend/src/http/controlers/orgs/routes.ts
+++ ignite/node_desafios/03-api-find-a-friend/src/http/controlers/orgs/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
+import { refresh } from "./refresh";
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post("/organization/register", register)
   app.post("/organization/session", authenticate)
+  app.patch("/token/refresh", refresh)
 }
