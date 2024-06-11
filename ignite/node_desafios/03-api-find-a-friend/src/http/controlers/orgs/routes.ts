dcc7ba987bc4ec8cbd5c2861e85d16ca16775b9e
Fabricio <fabricioliquesley15@gmail.com>
2024-06-11 10:00:08 -0300

    feat: create route to authenticate org

--- ignite/node_desafios/03-api-find-a-friend/src/http/controlers/orgs/routes.ts
+++ ignite/node_desafios/03-api-find-a-friend/src/http/controlers/orgs/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
+import { authenticate } from "./authenticate";
 
 export async function orgsRoutes(app: FastifyInstance) {
-  app.post("/register", register)
+  app.post("/organization/register", register)
+  app.post("/organization/session", authenticate)
 }
