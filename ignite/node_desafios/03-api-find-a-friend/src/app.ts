f16bb6eafe7beaab5608a41a5393108659abeed0
Fabricio <fabricioliquesley15@gmail.com>
2024-06-11 11:21:46 -0300

    feat: register fastifyJwt adn fastifyCookie

--- ignite/node_desafios/03-api-find-a-friend/src/app.ts
+++ ignite/node_desafios/03-api-find-a-friend/src/app.ts
@@ -2,8 +2,23 @@ import fastify from "fastify";
 import { orgsRoutes } from "./http/controlers/orgs/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.SECRET_JWT,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false
+  },
+  sign: {
+    expiresIn: "10m"
+  }
+})
+
+app.register(fastifyCookie)
 
 app.register(orgsRoutes)
 
