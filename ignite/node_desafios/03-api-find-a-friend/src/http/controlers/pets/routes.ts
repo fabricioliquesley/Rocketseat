a16692853f74e9928d82289c6b72aa62f04b3aef
Fabricio <fabricioliquesley15@gmail.com>
2024-06-12 11:15:58 -0300

    feat: add route to get details of a pet

--- ignite/node_desafios/03-api-find-a-friend/src/http/controlers/pets/routes.ts
+++ ignite/node_desafios/03-api-find-a-friend/src/http/controlers/pets/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { verifyJWT } from "@/http/middlewares/virify-jwt";
+import { getPetDetails } from "./get-pet-details";
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post("/pets/register", { onRequest: [verifyJWT] }, register);
+  app.get("/pets", getPetDetails);
 }
