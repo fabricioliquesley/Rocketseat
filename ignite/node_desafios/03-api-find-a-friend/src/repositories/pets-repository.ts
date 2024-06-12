95bb5f4874e335a4ac5d8c5c794c90c60629a680
Fabricio <fabricioliquesley15@gmail.com>
2024-06-12 10:18:27 -0300

    feat: add method findPetById in PetsRepository interface

--- ignite/node_desafios/03-api-find-a-friend/src/repositories/pets-repository.ts
+++ ignite/node_desafios/03-api-find-a-friend/src/repositories/pets-repository.ts
@@ -2,4 +2,5 @@ import { Pet, Prisma } from "@prisma/client";
 
 export interface PetsRepository {
   createPet(data: Prisma.PetUncheckedCreateInput): Promise<Pet>;
+  findPetById(id: string): Promise<Pet | null>;
 }
