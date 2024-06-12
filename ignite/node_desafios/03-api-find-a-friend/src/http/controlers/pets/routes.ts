bd904e897cf09ad56a4e9395f4f23bb1772bcbaf
Fabricio <fabricioliquesley15@gmail.com>
2024-06-12 09:42:01 -0300

    feat: create route to register pets

---
+++ ignite/node_desafios/03-api-find-a-friend/src/http/controlers/pets/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from "fastify";
+import { register } from "./register";
+import { verifyJWT } from "@/http/middlewares/virify-jwt";
+
+export async function petsRoutes(app: FastifyInstance) {
+  app.post("/pets/register", { onRequest: [verifyJWT] }, register);
+}
