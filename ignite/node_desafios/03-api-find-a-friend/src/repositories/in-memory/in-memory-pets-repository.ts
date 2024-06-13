2dac383185c9926d11a0d98ce4539c576b3966cf
Fabricio <fabricioliquesley15@gmail.com>
2024-06-13 09:18:31 -0300

    feat: implements the findPets method in InMemoryPetsRepository

--- ignite/node_desafios/03-api-find-a-friend/src/repositories/in-memory/in-memory-pets-repository.ts
+++ ignite/node_desafios/03-api-find-a-friend/src/repositories/in-memory/in-memory-pets-repository.ts
@@ -1,9 +1,10 @@
-import { Pet, Prisma } from "@prisma/client";
+import { Org, Pet, Prisma } from "@prisma/client";
 import { PetsRepository } from "../pets-repository";
 import { randomUUID } from "crypto";
 
 export class InMemoryPetsRepository implements PetsRepository {
   public items: Pet[] = [];
+  public orgs: Org[] = [];
 
   async createPet(data: Prisma.PetUncheckedCreateInput): Promise<Pet> {
     const pet: Pet = {
@@ -20,12 +21,24 @@ export class InMemoryPetsRepository implements PetsRepository {
   }
 
   async findPetById(id: string): Promise<Pet | null> {
-    const pet = this.items.find(pet => pet.id === id);
+    const pet = this.items.find((pet) => pet.id === id);
 
     if (!pet) {
       return null;
     }
 
-    return pet
+    return pet;
+  }
+
+  async findPets(city: string): Promise<Pet[] | null> {
+    const pets = this.items.filter((pet) => {
+      const index = this.orgs.findIndex((org) => org.address.includes(city));
+
+      return pet.orgId === this.orgs[index].id;
+    });
+
+    if (pets.length === 0) return null;
+
+    return pets;
   }
 }
