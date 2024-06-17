e17db12bc1d5107b7530611e2a0ce4e4d7c57fbd
Fabricio <fabricioliquesley15@gmail.com>
2024-06-17 09:31:45 -0300

    chore: create global styles

---
+++ ignite/react_desafios/D02-Coffee-Delivery/src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: ${props => props.theme["base-background"]};
+    font-size: 1.6rem;
+  }
+
+  body, input, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    padding: 16rem;
+  }
+`;
