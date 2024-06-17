e5aae7b6eef6c612c842896dc3360099ad933ed5
Fabricio <fabricioliquesley15@gmail.com>
2024-06-17 09:30:07 -0300

    feat: create a theme to project

---
+++ ignite/react_desafios/D02-Coffee-Delivery/src/styles/theme/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  "base-white": "#ffffff",
+  "base-background": "#fafafa",
+  "base-card": "#f3f2f2",
+  "base-input": "#ededed",
+  "base-button": "#e6e5e5",
+  "base-hover": "#d7d5d5",
+  "base-label": "#8d8686",
+  "base-text": "#574f4d",
+  "base-subtitle": "#403937",
+  "base-title": "#272221",
+  "purple-dark": "#4b2995",
+  "purple": "#8047f8",
+  "purple-light": "#ebe5f9",
+  "yellow-dark": "#c47f17",
+  "yellow": "#dbac2c",
+  "yellow-light": "#f1e9c9",
+};
