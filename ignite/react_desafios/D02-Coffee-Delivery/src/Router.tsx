dec0c0413d2953d334916489c2028fc656395535
Fabricio <fabricioliquesley15@gmail.com>
2024-06-19 10:13:11 -0300

    feat: create route to acess order success page

--- ignite/react_desafios/D02-Coffee-Delivery/src/Router.tsx
+++ ignite/react_desafios/D02-Coffee-Delivery/src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./pages/Home";
 import { CheckOut } from "./pages/CheckOut";
+import { Success } from "./pages/Success";
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<CheckOut />} />
+        <Route path="/success" element={<Success />} />
       </Route>
     </Routes>
   );
