7efeb0bd6b612c07b278a9c46069672995754853
Fabricio <fabricioliquesley15@gmail.com>
2024-06-19 10:14:15 -0300

    feat: defined the maximum width of the layout

--- ignite/react_desafios/D02-Coffee-Delivery/src/layouts/DefaultLayout/index.tsx
+++ ignite/react_desafios/D02-Coffee-Delivery/src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header";
+import { LayoutContent } from "./styles";
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContent>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContent>
   );
 }
---
+++ ignite/react_desafios/D02-Coffee-Delivery/src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const LayoutContent = styled.div`
+  max-width: 144rem;
+  margin: 0 auto;
+`
