3d280a87e6c630d6f46b5904e58b4edc071eeba5
Fabricio <fabricioliquesley15@gmail.com>
2024-06-20 15:29:26 -0300

    feat: use cartContext in the checkout page

--- ignite/react_desafios/D02-Coffee-Delivery/src/pages/CheckOut/index.tsx
+++ ignite/react_desafios/D02-Coffee-Delivery/src/pages/CheckOut/index.tsx
@@ -16,8 +16,74 @@ import {
 } from "./styles";
 import { Input } from "./components/Input";
 import { CoffeeItem } from "./components/CoffeeItem";
+import { useContext, useEffect, useState } from "react";
+import { CartContext } from "../../context/CartContext";
+import { coffees } from "../../utils/coffees";
+import { formatPrice } from "../../utils/formatPrice";
+import { getAddress, getAddressResponse } from "../../utils/getAddress";
+import { useNavigate } from "react-router-dom";
 
 export function CheckOut() {
+  const { selectedCoffees, clearCart } = useContext(CartContext);
+  const [address, setAddress] = useState<getAddressResponse>();
+  const [cep, setCep] = useState("");
+  const [number, setNumber] = useState("");
+  const [complement, setComplement] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("");
+
+  const selectedCoffeeIds = new Set(
+    selectedCoffees.map((selectedCoffee) => selectedCoffee.id)
+  );
+
+  const filteredCoffeesList = coffees
+    .filter((coffee) => selectedCoffeeIds.has(coffee.id))
+    .map((coffee) => {
+      const selectedCoffee = selectedCoffees.find((sc) => sc.id === coffee.id);
+      return {
+        ...coffee,
+        quantity: selectedCoffee ? selectedCoffee.quantity : 0,
+      };
+    });
+
+  const totalItemsPrice = filteredCoffeesList.reduce((acc, coffee) => {
+    const selectedCoffee = selectedCoffees.find((sc) => sc.id === coffee.id);
+    const quantity = selectedCoffee ? selectedCoffee.quantity : 0;
+
+    return acc + coffee.price * quantity;
+  }, 0);
+  const deliveryPrice = 3.5;
+  const totalPrice = totalItemsPrice + deliveryPrice;
+
+  const navigate = useNavigate();
+
+  function handleSubmit() {
+    const order = {
+      address: {
+        ...address,
+        number,
+        complement,
+      },
+      deliveryTime: 30,
+      paymentMethod,
+    };
+
+    localStorage.setItem("@coffee-delivery:order", JSON.stringify(order));
+    clearCart();
+    navigate("/success");
+  }
+
+  useEffect(() => {
+    if (cep.length === 8) {
+      getAddress(cep)
+        .then((address) => {
+          setAddress(address);
+        })
+        .catch((error) => {
+          console.error("Error fetching address:", error);
+        });
+    }
+  }, [cep]);
+
   return (
     <CheckoutContainer>
       <section>
@@ -31,13 +97,41 @@ export function CheckOut() {
             </div>
           </CardDescription>
           <form>
-            <Input placeholder="CEP" type="number" />
-            <Input placeholder="Rua" type="text" />
-            <Input placeholder="Número" type="number" />
-            <Input placeholder="Complemento" type="text" variant={"optional"} />
-            <Input placeholder="Bairro" type="text" />
-            <Input placeholder="Cidade" type="text" />
-            <Input placeholder="UF" type="text" />
+            <Input
+              placeholder="CEP"
+              type="number"
+              onChange={(e) => setCep(e.target.value)}
+            />
+            <Input
+              placeholder="Rua"
+              type="text"
+              value={address?.logradouro}
+              readOnly
+            />
+            <Input
+              placeholder="Número"
+              type="number"
+              onChange={(e) => setNumber(e.target.value)}
+            />
+            <Input
+              placeholder="Complemento"
+              type="text"
+              variant={"optional"}
+              onChange={(e) => setComplement(e.target.value)}
+            />
+            <Input
+              placeholder="Bairro"
+              type="text"
+              value={address?.bairro}
+              readOnly
+            />
+            <Input
+              placeholder="Cidade"
+              type="text"
+              value={address?.localidade}
+              readOnly
+            />
+            <Input placeholder="UF" type="text" value={address?.uf} readOnly />
           </form>
         </DeliveryFormCard>
         <PaymentMethodContainer>
@@ -50,19 +144,37 @@ export function CheckOut() {
           </CardDescription>
           <div className="paymentMethodOptions">
             <PaymentMethod>
-              <input type="radio" name="paymentMethod" id="creditCard" />
+              <input
+                type="radio"
+                name="paymentMethod"
+                id="creditCard"
+                value="creditCard"
+                onChange={(e) => setPaymentMethod(e.target.value)}
+              />
               <CreditCard size={16} />
               <p>Cartão de crédito</p>
               <div className="whenChecked"></div>
             </PaymentMethod>
             <PaymentMethod>
-              <input type="radio" name="paymentMethod" id="debitCard" />
+              <input
+                type="radio"
+                name="paymentMethod"
+                id="debitCard"
+                value="debitCard"
+                onChange={(e) => setPaymentMethod(e.target.value)}
+              />
               <Bank size={16} />
               <p>Cartão de debito</p>
               <div className="whenChecked"></div>
             </PaymentMethod>
             <PaymentMethod>
-              <input type="radio" name="paymentMethod" id="money" />
+              <input
+                type="radio"
+                name="paymentMethod"
+                id="money"
+                value="money"
+                onChange={(e) => setPaymentMethod(e.target.value)}
+              />
               <Money size={16} />
               <p>Dinheiro</p>
               <div className="whenChecked"></div>
@@ -73,25 +185,27 @@ export function CheckOut() {
       <section>
         <h3>Cafés selecionados</h3>
         <OrderDetails>
-          <CoffeeItem />
-          <Separator />
-          <CoffeeItem />
-          <Separator />
+          {filteredCoffeesList.map((coffee) => (
+            <>
+              <CoffeeItem coffeeData={coffee} key={coffee.id} />
+              <Separator />
+            </>
+          ))}
           <div className="values">
             <div>
               <p>Total de itens</p>
-              <span>R$ 29,70</span>
+              <span>R$ {formatPrice(totalItemsPrice)}</span>
             </div>
             <div>
               <p>Entrega</p>
-              <span>R$ 3,50</span>
+              <span>R$ {formatPrice(deliveryPrice)}</span>
             </div>
             <div>
               <h3>Total</h3>
-              <h3>R$ 33,50</h3>
+              <h3>R$ {formatPrice(totalPrice)}</h3>
             </div>
           </div>
-          <button>confirmar pedido</button>
+          <button onClick={handleSubmit}>confirmar pedido</button>
         </OrderDetails>
       </section>
     </CheckoutContainer>
