16ef9d3ef6765b50f016c058f3f62a0bebc57a1a
Fabricio <fabricioliquesley15@gmail.com>
2024-06-20 15:28:16 -0300

    feat: use cartContext in the coffeeItem component on the home page

--- ignite/react_desafios/D02-Coffee-Delivery/src/pages/Home/components/CoffeeItem/index.tsx
+++ ignite/react_desafios/D02-Coffee-Delivery/src/pages/Home/components/CoffeeItem/index.tsx
@@ -1,6 +1,9 @@
+import { useContext, useState } from "react";
 import { Badge, BadgeList, ContentContainer, Controls } from "./styles";
 
 import { Minus, Plus, ShoppingCart } from "@phosphor-icons/react";
+import { CartContext } from "../../../../context/CartContext";
+import { formatPrice } from "../../../../utils/formatPrice";
 
 export type coffeeProps = {
   id: string;
@@ -16,30 +19,49 @@ interface CoffeeItemProps {
 }
 
 export function CoffeeItem({ coffeeData }: CoffeeItemProps) {
+  const { addCoffee } = useContext(CartContext);
+  const [coffeeQuantity, setCoffeeQuantity] = useState(1);
+
+  function handleDecreaseQuantity() {
+    if (coffeeQuantity === 0) return;
+
+    setCoffeeQuantity((prev) => prev - 1);
+  }
+
+  function handleIncreaseQuantity() {
+    setCoffeeQuantity((prev) => prev + 1);
+  }
+
+  function handleAddCoffee() {
+    addCoffee(coffeeData.id, coffeeQuantity);
+  }
+
   return (
     <ContentContainer>
       <img src={coffeeData.imgURL} alt="Imagem ilustrativa do café" />
       <BadgeList>
-        {coffeeData.tags.map(tag => <Badge>{tag}</Badge>)}
+        {coffeeData.tags.map((tag) => (
+          <Badge key={tag}>{tag}</Badge>
+        ))}
       </BadgeList>
       <h3>{coffeeData.title}</h3>
       <p>{coffeeData.description}</p>
       <Controls>
         <h4>
           <b>R$ </b>
-          {coffeeData.price.toString().replace(".", ",")}
+          {formatPrice(coffeeData.price)}
         </h4>
         <div>
           <div>
-            <button>
+            <button onClick={handleDecreaseQuantity}>
               <Minus />
             </button>
-            <span>1</span>
-            <button>
+            <span>{coffeeQuantity}</span>
+            <button onClick={handleIncreaseQuantity}>
               <Plus />
             </button>
           </div>
-          <button>
+          <button onClick={handleAddCoffee}>
             <ShoppingCart weight="fill" size={22} />
           </button>
         </div>
