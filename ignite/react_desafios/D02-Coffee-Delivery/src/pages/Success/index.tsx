5ae0808b252e86fa5fd357328dd4fdfe75b574da
Fabricio <fabricioliquesley15@gmail.com>
2024-06-20 15:31:06 -0300

    feat: use cartContext on the success page

--- ignite/react_desafios/D02-Coffee-Delivery/src/pages/Success/index.tsx
+++ ignite/react_desafios/D02-Coffee-Delivery/src/pages/Success/index.tsx
@@ -8,42 +8,80 @@ import {
 
 import deliveryMan from "../../assets/deliveryMan.svg";
 import { CurrencyDollar, MapPin, Timer } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
+import { getAddressResponse } from "../../utils/getAddress";
+
+type addressWithNumberAndComplement = getAddressResponse & {
+  number: string;
+  complement: string;
+};
+
+interface OrderProps {
+  address: addressWithNumberAndComplement;
+  deliveryTime: number;
+  paymentMethod: "money" | "debitCard" | "creditCard";
+}
+
+const PAYMENT_METHOD = {
+  money: "Dinheiro",
+  debitCard: "Cartão de debito",
+  creditCard: "Cartão de credito",
+};
 
 export function Success() {
+  const [order, setOrder] = useState<OrderProps | null>(null);
+  useEffect(() => {
+    const orderAsJSON = localStorage.getItem("@coffee-delivery:order");
+
+    if (orderAsJSON) {
+      setOrder(JSON.parse(orderAsJSON));
+    }
+  }, []);
+
   return (
     <ContentContainer>
-      <OrderDetails>
-        <h2>Uhu! Pedido confirmado</h2>
-        <p>Agora é só aguardar que logo o café chegará até você</p>
-        <div className="gradient">
-          <div>
-            <LocationBox>
+      {!order ? (
+        <h2>Não existe nem um pedido em andamento</h2>
+      ) : (
+        <>
+          <OrderDetails>
+            <h2>Uhu! Pedido confirmado</h2>
+            <p>Agora é só aguardar que logo o café chegará até você</p>
+            <div className="gradient">
               <div>
-                <MapPin size={16} weight="fill" />
+                <LocationBox>
+                  <div>
+                    <MapPin size={16} weight="fill" />
+                  </div>
+                  <p>
+                    Entrega em <b>{order.address.logradouro}</b>,{" "}
+                    {order.address.number} {order.address.bairro} -{" "}
+                    {order.address.localidade}, {order.address.uf}
+                  </p>
+                </LocationBox>
+                <DeliveryForecastBox>
+                  <div>
+                    <Timer size={16} weight="fill" />
+                  </div>
+                  <p>
+                    Previsão de entrega <b>{order.deliveryTime} min</b>
+                  </p>
+                </DeliveryForecastBox>
+                <PaymentMethodBox>
+                  <div>
+                    <CurrencyDollar size={16} />
+                  </div>
+                  <p>
+                    Pagamento na entrega{" "}
+                    <b>{PAYMENT_METHOD[order.paymentMethod]}</b>
+                  </p>
+                </PaymentMethodBox>
               </div>
-              <p>
-                Entrega em <b>Rua João Daniel Martinelli</b>, 102 Farrapos -
-                Porto Alegre, RS
-              </p>
-            </LocationBox>
-            <DeliveryForecastBox>
-              <div>
-                <Timer size={16} weight="fill" />
-              </div>
-              <p>
-                Previsão de entrega <b>20 min - 30 min</b>
-              </p>
-            </DeliveryForecastBox>
-            <PaymentMethodBox>
-              <div>
-                <CurrencyDollar size={16} />
-              </div>
-              <p>Pagamento na entrega <b>Cartão de Crédito</b></p>
-            </PaymentMethodBox>
-          </div>
-        </div>
-      </OrderDetails>
-      <img src={deliveryMan} alt="delivery man illustration" />
+            </div>
+          </OrderDetails>
+          <img src={deliveryMan} alt="delivery man illustration" />
+        </>
+      )}
     </ContentContainer>
   );
 }
