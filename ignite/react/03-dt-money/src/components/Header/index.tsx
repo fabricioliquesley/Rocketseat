74de6761feeb610770091c62283dc5be7631cad3
Fabricio <fabricioliquesley15@gmail.com>
2024-06-25 11:27:19 -0300

    feat: open modal from Header component

--- ignite/react/03-dt-money/src/components/Header/index.tsx
+++ ignite/react/03-dt-money/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 import logoURL from "../../assets/logo.svg";
 
@@ -6,13 +8,13 @@ export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img
-          src={logoURL}
-          alt=""
-        />
-        <NewTransactionButton>
-          Nova transação
-        </NewTransactionButton>
+        <img src={logoURL} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
