9c7d9d14dfee13a568a125791930e931e6e04606
Fabricio <fabricioliquesley15@gmail.com>
2024-07-01 10:08:45 -0300

    feat: create router for the pages

--- ignite/react_desafios/D03-Github-Blog/src/App.tsx
+++ ignite/react_desafios/D03-Github-Blog/src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/defaultTheme";
 import { GlobalStyled } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyled />
     </ThemeProvider>
   );
