1a1314dd8a5bf6a45af20a80ed6fb40d339c536f
Fabricio <fabricioliquesley15@gmail.com>
2024-07-01 10:19:11 -0300

    feat: create the Header component

---
+++ ignite/react_desafios/D03-Github-Blog/src/components/Header/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+import headerBackground from "../../assets/Cover.png"
+
+export const HeaderContainer = styled.header`
+  height: 29.6rem;
+  background: url(${headerBackground}) center /cover no-repeat;
+`;
