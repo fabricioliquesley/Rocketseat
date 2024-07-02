270eece0b870cc14e0c2e7319f197c0fe8da59f3
Fabricio <fabricioliquesley15@gmail.com>
2024-07-02 11:46:07 -0300

    feat: create a test of a text-to-slug conversion

---
+++ ignite/node/04-clean-ddd/src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("it should be able to create a ne slug from text", () => {
+  const slug = Slug.createFromText("An Example Title");
+
+  expect(slug.value).toEqual("an-example-title");
+});
