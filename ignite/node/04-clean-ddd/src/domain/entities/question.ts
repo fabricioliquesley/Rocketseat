fccf6b9288977068908a0c5949eaa24d32d4c79a
Fabricio <fabricioliquesley15@gmail.com>
2024-07-02 11:51:52 -0300

    refact: adapt the answer-question use case to work with the repository

--- ignite/node/04-clean-ddd/src/domain/entities/question.ts
+++ ignite/node/04-clean-ddd/src/domain/entities/question.ts
@@ -1,6 +1,8 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
+  slug: Slug;
   title: string;
   content: string;
   authorId: string;
@@ -8,14 +10,16 @@ interface QuestionProps {
 
 export class Question {
   public id: string;
+  public slug: Slug;
   public title: string;
   public content: string;
   public authorId: string;
 
-  constructor({ title, content, authorId }: QuestionProps, id?: string) {
-    this.title = title;
-    this.content = content;
-    this.authorId = authorId;
+  constructor(props: QuestionProps, id?: string) {
+    this.slug = props.slug;
+    this.title = props.title;
+    this.content = props.content;
+    this.authorId = props.authorId;
     this.id = id ?? randomUUID();
   }
 }
