9ac9b1128bffe9607d2f995384d3e04434e69917
Fabricio <fabricioliquesley15@gmail.com>
2024-07-02 11:50:42 -0300

    test: adapt the answer-question to work with the repository

--- ignite/node/04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
+++ ignite/node/04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,17 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer) => {
+    return
+  },
+}
 
-  const answer = answerQuestion.execute({
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+  const answer = await answerQuestion.execute({
     instructorId: "XX01",
     questionId: "XX02",
     content: "New Answer",
