486a2908d001899d11b78693bc510141d41a5e25
Fabricio <fabricioliquesley15@gmail.com>
2024-07-03 14:26:02 -0300

    styles: add styles from the Input component to SearchComponent

---
+++ ignite/react_desafios/D03-Github-Blog/src/pages/Home/components/SearchForm/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.form`
+  & > input {
+    width: 100%;
+    background: ${(props) => props.theme["base-input"]};
+    border: 1px solid ${(props) => props.theme["base-border"]};
+    border-radius: 0.6rem;
+    padding: 1.2rem 1.6rem;
+    outline: none;
+
+    &,
+    &::placeholder {
+      color: ${(props) => props.theme["base-label"]};
+      line-height: 160%;
+    }
+
+    &:focus {
+      border-color: ${(props) => props.theme.blue};
+
+      &,
+      &::placeholder {
+        color: ${(props) => props.theme["base-text"]};
+      }
+    }
+  }
+`;
