fc8e61c616669548ad320c45904222efdc405d29
Fabricio <fabricioliquesley15@gmail.com>
2024-07-13 15:40:13 -0300

    feat: create a use case to comment on the question

---
+++ ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { makeQuestion } from "test/factories/make-question";
+import { CommentOnQuestionUseCase } from "./comment-on-question";
+import { InMemoryQuestionCommentsRepository } from "test/repositories/in-memory-question-comments-respository";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: CommentOnQuestionUseCase;
+
+beforeEach(() => {
+  inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+  inMemoryQuestionCommentsRepository = new InMemoryQuestionCommentsRepository();
+
+  sut = new CommentOnQuestionUseCase(
+    inMemoryQuestionsRepository,
+    inMemoryQuestionCommentsRepository
+  );
+});
+
+describe("Comment on Question", () => {
+  it("should be able to comment on question", async () => {
+    const fakeQuestion = makeQuestion();
+
+    await inMemoryQuestionsRepository.create(fakeQuestion);
+
+    await sut.execute({
+      questionId: fakeQuestion.id.toString(),
+      authorId: "SX01",
+      content: "New comment",
+    });
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toBe(
+      "New comment"
+    );
+  });
+});
