bf8beb368ab574031ccf372bcfbe479a884d022e
Fabricio <fabricioliquesley15@gmail.com>
2024-07-13 15:39:41 -0300

    feat: create a use case to comment on the answer

---
+++ ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { makeAnswer } from "test/factories/make-answer";
+import { CommentOnAnswerUseCase } from "./comment-on-answer";
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-respository";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: CommentOnAnswerUseCase;
+
+beforeEach(() => {
+  inMemoryAnswersRepository = new InMemoryAnswersRepository();
+  inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+
+  sut = new CommentOnAnswerUseCase(
+    inMemoryAnswersRepository,
+    inMemoryAnswerCommentsRepository
+  );
+});
+
+describe("Comment on Answer", () => {
+  it("should be able to comment on answer", async () => {
+    const fakeAnswer = makeAnswer();
+
+    await inMemoryAnswersRepository.create(fakeAnswer);
+
+    await sut.execute({
+      answerId: fakeAnswer.id.toString(),
+      authorId: "SX01",
+      content: "New comment",
+    });
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toBe(
+      "New comment"
+    );
+  });
+});
