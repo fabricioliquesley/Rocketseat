f1e2f02646ca8c6e8454cb806f93d8a600237979
Fabricio <fabricioliquesley15@gmail.com>
2024-07-13 15:42:41 -0300

    feat: create a use case to list answer comments

---
+++ ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/list-answer-comments.spec.ts
@@ -0,0 +1,47 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-respository";
+import { ListAnswerCommentsUseCase } from "./list-answer-comments";
+import { makeAnswerComment } from "test/factories/make-answer-comment";
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: ListAnswerCommentsUseCase;
+
+describe("List Answer Comments", () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository =
+      new InMemoryAnswerCommentsRepository();
+    sut = new ListAnswerCommentsUseCase(inMemoryAnswerCommentsRepository);
+  });
+
+  it("Should be able to list answer comments", async () => {
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({ answerId: new UniqueEntityId("QX01") })
+    );
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({ answerId: new UniqueEntityId("QX01") })
+    );
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({ answerId: new UniqueEntityId("QX01") })
+    );
+
+    const { answerComments } = await sut.execute({
+      answerId: "QX01",
+      page: 1,
+    });
+
+    expect(answerComments).toHaveLength(3);
+  });
+
+  it("Should be able to list paginated answer comments", async () => {
+    for (let i = 0; i < 22; i++) {
+      await inMemoryAnswerCommentsRepository.create(
+        makeAnswerComment({ answerId: new UniqueEntityId("QX02") })
+      );
+    }
+
+    const { answerComments } = await sut.execute({ answerId: "QX02", page: 2 });
+
+    expect(answerComments).toHaveLength(2);
+  });
+});
