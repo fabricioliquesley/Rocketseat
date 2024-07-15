07ed9d7d23d0dd1ea5d712c4eb54261a9b3471d6
Fabricio <fabricioliquesley15@gmail.com>
2024-07-15 14:38:53 -0300

    feat: allows to edit attachments when editing the question

--- ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/edit-question.spec.ts
+++ ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -4,32 +4,65 @@ import { EditQuestionUseCase } from "./edit-question";
 import { makeQuestion } from "test/factories/make-question";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { makeQuestionAttachment } from "test/factories/make-question-attachment";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
 let sut: EditQuestionUseCase;
 
 beforeEach(() => {
-  inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
-  sut = new EditQuestionUseCase(inMemoryQuestionsRepository);
+  inMemoryQuestionAttachmentsRepository =
+    new InMemoryQuestionAttachmentsRepository();
+  inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+    inMemoryQuestionAttachmentsRepository
+  );
+  sut = new EditQuestionUseCase(
+    inMemoryQuestionsRepository,
+    inMemoryQuestionAttachmentsRepository
+  );
 });
 
 describe("Edit Question", () => {
   it("should be able to edit a question", async () => {
     const authorId = new UniqueEntityId("SX01");
     const fakeQuestion = makeQuestion({ authorId }, new UniqueEntityId("QX01"));
+
     await inMemoryQuestionsRepository.create(fakeQuestion);
+    await inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: fakeQuestion.id,
+        attachmentId: new UniqueEntityId("QAX01"),
+      }),
+      makeQuestionAttachment({
+        questionId: fakeQuestion.id,
+        attachmentId: new UniqueEntityId("QAX02"),
+      })
+    );
 
     const { value } = await sut.execute({
       questionId: fakeQuestion.id.toString(),
       authorId: "SX01",
       title: "New title",
       content: "New content",
+      attachmentsIds: ["QAX01", "QAX03"],
     });
 
-    expect(value?.question).toMatchObject({
-      title: "New title",
-      content: "New content",
+    expect(value).toMatchObject({
+      question: expect.objectContaining({
+        title: "New title",
+        content: "New content",
+      }),
     });
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.getItems()
+    ).toHaveLength(2);
+    expect(inMemoryQuestionsRepository.items[0].attachments.getItems()).toEqual(
+      [
+        expect.objectContaining({ attachmentId: new UniqueEntityId("QAX01") }),
+        expect.objectContaining({ attachmentId: new UniqueEntityId("QAX03") }),
+      ]
+    );
   });
 
   it("it shouldn't be possible to edit a question if you're not the author", async () => {
@@ -42,9 +75,10 @@ describe("Edit Question", () => {
       authorId: "SX02",
       title: "New title",
       content: "New content",
-    })
+      attachmentsIds: [],
+    });
 
-    expect(result.isLeft()).toBe(true)
-    expect(result.value).toBeInstanceOf(NotAllowedError)
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
