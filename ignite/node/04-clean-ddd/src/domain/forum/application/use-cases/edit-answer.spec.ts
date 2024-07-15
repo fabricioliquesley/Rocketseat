84543629a6c9252d028fcf765cc4ce9393d18e3d
Fabricio <fabricioliquesley15@gmail.com>
2024-07-15 14:38:33 -0300

    feat: allows to edit attachments when editing the answer

--- ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/edit-answer.spec.ts
+++ ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -4,31 +4,62 @@ import { EditAnswerUseCase } from "./edit-answer";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { makeAnswer } from "test/factories/make-answer";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { InMemoryAnswerAttachmentsRepository } from "test/repositories/in-memory-answer-attachments-repository";
+import { makeAnswerAttachment } from "test/factories/make-answer-attachment";
 
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
 let inMemoryAnswerRepository: InMemoryAnswersRepository;
 let sut: EditAnswerUseCase;
 
 beforeEach(() => {
-  inMemoryAnswerRepository = new InMemoryAnswersRepository();
-  sut = new EditAnswerUseCase(inMemoryAnswerRepository);
+  inMemoryAnswerAttachmentsRepository =
+    new InMemoryAnswerAttachmentsRepository();
+  inMemoryAnswerRepository = new InMemoryAnswersRepository(
+    inMemoryAnswerAttachmentsRepository
+  );
+  sut = new EditAnswerUseCase(
+    inMemoryAnswerRepository,
+    inMemoryAnswerAttachmentsRepository
+  );
 });
 
 describe("Edit Answer", () => {
   it("should be able to edit a answer", async () => {
     const authorId = new UniqueEntityId("IX01");
     const fakeAnswer = makeAnswer({ authorId }, new UniqueEntityId("AX01"));
+
     await inMemoryAnswerRepository.create(fakeAnswer);
+    await inMemoryAnswerAttachmentsRepository.items.push(
+      makeAnswerAttachment({
+        answerId: fakeAnswer.id,
+        attachmentId: new UniqueEntityId("QAX01"),
+      }),
+      makeAnswerAttachment({
+        answerId: fakeAnswer.id,
+        attachmentId: new UniqueEntityId("QAX02"),
+      })
+    );
 
     const result = await sut.execute({
       answerId: "AX01",
       authorId: authorId.toString(),
+      attachmentsIds: ["AAX01", "AAX03"],
       content: "New answer content",
     });
 
-    expect(result.isRight()).toBe(true)
-    expect(result.value?.answer).toMatchObject({
-      content: "New answer content",
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toMatchObject({
+      answer: expect.objectContaining({
+        content: "New answer content",
+      }),
     });
+    expect(
+      inMemoryAnswerRepository.items[0].attachments.getItems()
+    ).toHaveLength(2);
+    expect(inMemoryAnswerRepository.items[0].attachments.getItems()).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId("AAX01") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId("AAX03") }),
+    ]);
   });
 
   it("it shouldn't be possible to edit a answer if you're not the author", async () => {
@@ -39,6 +70,7 @@ describe("Edit Answer", () => {
     const result = await sut.execute({
       answerId: "AX01",
       authorId: "IX02",
+      attachmentsIds: [],
       content: "New content",
     });
 
--- ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/edit-answer.ts
+++ ignite/node/04-clean-ddd/src/domain/forum/application/use-cases/edit-answer.ts
@@ -3,10 +3,15 @@ import { Answer } from "../../enterprise/entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { AnswerAttachment } from "../../enterprise/entities/answer-attachment";
+import { AnswerAttachmentList } from "../../enterprise/entities/answer-attachment-list";
+import { AnswerAttachmentsRepository } from "../repositories/answer-attachments-repository";
 
 interface EditAnswerRequest {
   answerId: string;
   authorId: string;
+  attachmentsIds: string[];
   content: string;
 }
 
@@ -16,11 +21,15 @@ type EditAnswerResponse = Either<
 >;
 
 export class EditAnswerUseCase {
-  constructor(private answerRepository: AnswersRepository) {}
+  constructor(
+    private answerRepository: AnswersRepository,
+    private answerAttachmentsRepository: AnswerAttachmentsRepository
+  ) {}
 
   async execute({
     answerId,
     authorId,
+    attachmentsIds,
     content,
   }: EditAnswerRequest): Promise<EditAnswerResponse> {
     const answer = await this.answerRepository.findById(answerId);
@@ -33,7 +42,24 @@ export class EditAnswerUseCase {
       return left(new NotAllowedError());
     }
 
+    const currentAnswerAttachments =
+      await this.answerAttachmentsRepository.findManyByAnswerId(answerId);
+
+    const answerAttachmentList = new AnswerAttachmentList(
+      currentAnswerAttachments
+    );
+
+    const answerAttachments = attachmentsIds.map((attachmentId) => {
+      return AnswerAttachment.create({
+        attachmentId: new UniqueEntityId(attachmentId),
+        answerId: answer.id,
+      });
+    });
+
+    answerAttachmentList.update(answerAttachments);
+
     answer.content = content;
+    answer.attachments = answerAttachmentList;
 
     await this.answerRepository.save(answer);
 
