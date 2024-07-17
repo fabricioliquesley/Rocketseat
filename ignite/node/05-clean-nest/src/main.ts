05169db92e03a215954f011f6bac8cb14eddef99
Fabricio <fabricioliquesley15@gmail.com>
2024-07-17 12:19:04 -0300

    feat: configures the use of environment variables in main.ts

--- ignite/node/05-clean-nest/src/main.ts
+++ ignite/node/05-clean-nest/src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { Env } from "./env";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    // logger: false
+  });
+
+  const configureService = app.get<ConfigService<Env, true>>(ConfigService);
+  const port = configureService.get("PORT", {infer: true});
+
+  await app.listen(port);
 }
 bootstrap();
