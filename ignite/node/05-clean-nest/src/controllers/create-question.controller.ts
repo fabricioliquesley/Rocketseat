6b9e68f72dc0fc0dc0539ba84a4d626e2dd22c21
Fabricio <fabricioliquesley15@gmail.com>
2024-07-18 09:51:54 -0300

    feat: creates a controller to create a new question

---
+++ ignite/node/05-clean-nest/src/controllers/create-question.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { CurrentUser } from "src/auth/current-user-decorator";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UserPayload } from "src/auth/jwt.strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const requestBodySchema = z.object({
+  title: z.string().max(40),
+  content: z.string(),
+});
+
+type RequestBodySchema = z.infer<typeof requestBodySchema>;
+
+@Controller("/questions")
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(requestBodySchema)) body: RequestBodySchema
+  ) {
+    const { title, content } = body;
+    const authorId = user.sub;
+
+    const slug = this.convertToSlug(title);
+
+    await this.prisma.question.create({
+      data: {
+        title,
+        slug,
+        content,
+        authorId,
+      },
+    });
+  }
+
+  private convertToSlug(text: string): string {
+    return text
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^\w\s-]/g, "")
+      .replace(/\s+/g, "-");
+  }
+}
