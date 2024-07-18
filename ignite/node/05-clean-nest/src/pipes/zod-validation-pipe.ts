836fce31fa36f30261b85f926aefa530dfcd4661
Fabricio <fabricioliquesley15@gmail.com>
2024-07-18 09:51:04 -0300

    feat: adapts ZodValidationPipe to accept any zod schema and to accept type transformation

--- ignite/node/05-clean-nest/src/pipes/zod-validation-pipe.ts
+++ ignite/node/05-clean-nest/src/pipes/zod-validation-pipe.ts
@@ -1,13 +1,13 @@
 import { PipeTransform, BadRequestException } from "@nestjs/common";
-import { ZodError, ZodObject } from "zod";
+import { ZodError, ZodSchema } from "zod";
 import { fromZodError } from "zod-validation-error";
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodObject<any>) {}
+  constructor(private schema: ZodSchema) {}
 
   transform(value: unknown) {
     try {
-      this.schema.parse(value);
+      return this.schema.parse(value);
     } catch (error) {
       if (error instanceof ZodError) {
         throw new BadRequestException({
