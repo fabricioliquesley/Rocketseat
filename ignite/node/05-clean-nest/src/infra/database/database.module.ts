1084fde9c25c8414b6768ce8cde9744da8d63f07
Fabricio <fabricioliquesley15@gmail.com>
2024-07-19 19:12:19 -0300

    feat: add respositories in database module

--- ignite/node/05-clean-nest/src/infra/database/database.module.ts
+++ ignite/node/05-clean-nest/src/infra/database/database.module.ts
@@ -1,8 +1,34 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
+import { PrismaQuestionsRepository } from "./prisma/repositories/prisma-questions-repository";
+import { PrismaQuestionCommentsRepository } from "./prisma/repositories/prisma-question-comments-repository";
+import { PrismaQuestionAttachmentsRepository } from "./prisma/repositories/prisma-question-attachments-repository";
+import { PrismaAnswerAttachmentsRepository } from "./prisma/repositories/prisma-answer-attachments-repository";
+import { PrismaAnswerCommentsRepository } from "./prisma/repositories/prisma-answer-comments-repository";
+import { PrismaAnswersRepository } from "./prisma/repositories/prisma-answers-repository";
+import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: QuestionsRepository,
+      useClass: PrismaQuestionsRepository,
+    },
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+  ],
+  exports: [
+    PrismaService,
+    QuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+  ],
 })
 export class DatabaseModule {}
