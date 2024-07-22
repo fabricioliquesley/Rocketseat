0f846f9fcc614a00021309903907ab6d99a506f7
Fabricio <fabricioliquesley15@gmail.com>
2024-07-22 11:05:34 -0300

    feat: create the class questionFactory in make-question.ts

--- ignite/node/05-clean-nest/test/factories/make-question.ts
+++ ignite/node/05-clean-nest/test/factories/make-question.ts
@@ -5,18 +5,40 @@ import {
   Question,
   QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaQuestionsMapper } from "@/infra/database/prisma/mappers/prisma-question-mapper";
 
 export function makeQuestion(
   override?: Partial<QuestionProps>,
   id?: UniqueEntityId
 ) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: faker.lorem.sentence(),
-    content: faker.lorem.text(),
-    ...override,
-  }, id);
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id
+  );
 
   return question;
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {}
+  ): Promise<Question> {
+    const question = makeQuestion(data);
+
+    await this.prisma.question.create({
+      data: PrismaQuestionsMapper.toPrisma(question),
+    });
+
+    return question;
+  }
 }
