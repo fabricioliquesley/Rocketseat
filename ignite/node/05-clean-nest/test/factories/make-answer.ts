eb3a05aac6cb84cc74ab74eae90f28abcfa85300
Fabricio <fabricioliquesley15@gmail.com>
2024-07-22 11:38:35 -0300

    feat: create the answer factory

--- ignite/node/05-clean-nest/test/factories/make-answer.ts
+++ ignite/node/05-clean-nest/test/factories/make-answer.ts
@@ -2,12 +2,15 @@ import { faker } from "@faker-js/faker";
 
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Answer, AnswerProps } from "@/domain/forum/enterprise/entities/answer";
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaAnswerMapper } from "@/infra/database/prisma/mappers/prisma-answer-mapper";
 
 export function makeAnswer(
   override?: Partial<AnswerProps>,
   id?: UniqueEntityId
 ) {
-  const question = Answer.create(
+  const answer = Answer.create(
     {
       authorId: new UniqueEntityId(),
       questionId: new UniqueEntityId(),
@@ -17,5 +20,22 @@ export function makeAnswer(
     id
   );
 
-  return question;
+  return answer;
+}
+
+@Injectable()
+export class AnswerFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswer(
+    data: Partial<AnswerProps> = {}
+  ): Promise<Answer> {
+    const answer = makeAnswer(data);
+
+    await this.prisma.answer.create({
+      data: PrismaAnswerMapper.toPrisma(answer),
+    });
+
+    return answer;
+  }
 }
