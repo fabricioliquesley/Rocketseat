e55692b6991e3ea31600e8909d8c2b64cd258c23
Fabricio <fabricioliquesley15@gmail.com>
2024-07-22 12:20:13 -0300

    feat: create the controller to edit a question

---
+++ ignite/node/05-clean-nest/src/infra/http/controllers/edit-question.controller.ts
@@ -0,0 +1,48 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+} from "@nestjs/common";
+import { CurrentUser } from "src/infra/auth/current-user-decorator";
+import { UserPayload } from "src/infra/auth/jwt.strategy";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { z } from "zod";
+import { EditQuestionUseCase } from "@/domain/forum/application/use-cases/edit-question";
+
+const requestBodySchema = z.object({
+  title: z.string().max(40),
+  content: z.string(),
+});
+
+type RequestBodySchema = z.infer<typeof requestBodySchema>;
+
+@Controller("/questions/:id")
+export class EditQuestionController {
+  constructor(private editQuestion: EditQuestionUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(requestBodySchema)) body: RequestBodySchema,
+    @Param("id") questionId: string
+  ) {
+    const { title, content } = body;
+    const authorId = user.sub;
+
+    const result = await this.editQuestion.execute({
+      title,
+      content,
+      authorId,
+      attachmentsIds: [],
+      questionId,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+  }
+}
