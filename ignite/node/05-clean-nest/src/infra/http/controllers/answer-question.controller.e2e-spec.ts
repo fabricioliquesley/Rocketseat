e6911e20528a2d2af085ea7fe4af1befd376d840
Fabricio <fabricioliquesley15@gmail.com>
2024-07-23 10:15:41 -0300

    feat: create a test to the controller answerQuestionController

---
+++ ignite/node/05-clean-nest/src/infra/http/controllers/answer-question.controller.e2e-spec.ts
@@ -0,0 +1,59 @@
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { QuestionFactory } from "test/factories/make-question";
+import { StudentFactory } from "test/factories/make-student";
+
+describe("Answer Question (E2E)", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let studentFactory: StudentFactory;
+  let questionFactory: QuestionFactory;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const noduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile();
+
+    app = noduleRef.createNestApplication();
+    prisma = noduleRef.get(PrismaService);
+    studentFactory = noduleRef.get(StudentFactory);
+    questionFactory = noduleRef.get(QuestionFactory);
+    jwt = noduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test("[POST] /questions/:questionId/answers", async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    });
+
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const response = await request(app.getHttpServer())
+      .post(`/questions/${question.id.toString()}/answers`)
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        content: "Use the :hover pseudo-class to do this",
+      });
+
+    expect(response.statusCode).toBe(201);
+
+    const answerOnDataBase = await prisma.answer.findFirst({
+      where: {
+        authorId: user.id.toString(),
+      },
+    });
+
+    expect(answerOnDataBase).toBeTruthy();
+  });
+});
