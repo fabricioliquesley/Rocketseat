9f7bbfefdf75f6568540740ce36b66304cb96f51
Fabricio <fabricioliquesley15@gmail.com>
2024-07-23 10:20:33 -0300

    feat: create a controller to edit a answer

---
+++ ignite/node/05-clean-nest/src/infra/http/controllers/edit-answer.controller.ts
@@ -0,0 +1,46 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+} from "@nestjs/common";
+import { CurrentUser } from "src/infra/auth/current-user-decorator";
+import { UserPayload } from "src/infra/auth/jwt.strategy";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { z } from "zod";
+import { EditAnswerUseCase } from "@/domain/forum/application/use-cases/edit-answer";
+
+const requestBodySchema = z.object({
+  content: z.string(),
+});
+
+type RequestBodySchema = z.infer<typeof requestBodySchema>;
+
+@Controller("/answers/:id")
+export class EditAnswerController {
+  constructor(private editAnswer: EditAnswerUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(requestBodySchema)) body: RequestBodySchema,
+    @Param("id") answerId: string
+  ) {
+    const { content } = body;
+    const authorId = user.sub;
+
+    const result = await this.editAnswer.execute({
+      content,
+      authorId,
+      attachmentsIds: [],
+      answerId,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+  }
+}
