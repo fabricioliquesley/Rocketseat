bde7c7dab3c688d5215bd7712bce7d92e1bfabe6
Fabricio <fabricioliquesley15@gmail.com>
2024-07-23 10:11:46 -0300

    feat: create controller to answer a question

---
+++ ignite/node/05-clean-nest/src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,44 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+} from "@nestjs/common";
+import { CurrentUser } from "src/infra/auth/current-user-decorator";
+import { UserPayload } from "src/infra/auth/jwt.strategy";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { z } from "zod";
+import { AnswerQuestionUseCase } from "@/domain/forum/application/use-cases/answer-question";
+
+const requestBodySchema = z.object({
+  content: z.string(),
+});
+
+type RequestBodySchema = z.infer<typeof requestBodySchema>;
+
+@Controller("/questions/:questionId/answers")
+export class AnswerQuestionController {
+  constructor(private answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(requestBodySchema)) body: RequestBodySchema,
+    @Param("questionId") questionId: string
+  ) {
+    const { content } = body;
+    const authorId = user.sub;
+
+    const result = await this.answerQuestion.execute({
+      content,
+      questionId,
+      authorId,
+      attachmentsIds: [],
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+  }
+}
