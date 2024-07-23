01a690603b9ac7365f2c2d078c452584b1b38e14
Fabricio <fabricioliquesley15@gmail.com>
2024-07-23 11:46:29 -0300

    feat: create controller to list answer comments

---
+++ ignite/node/05-clean-nest/src/infra/http/controllers/fetch-answer-comments.controller.ts
@@ -0,0 +1,43 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from "@nestjs/common";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { z } from "zod";
+import { ListAnswerCommentsUseCase } from "@/domain/forum/application/use-cases/list-answer-comments";
+import { CommentPresenter } from "../presenters/comment-presenter";
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default("1")
+  .transform(Number)
+  .pipe(z.number().min(1));
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
+
+@Controller("/answers/:answerId/comments")
+export class FetchAnswerCommentsController {
+  constructor(private listAnswerComments: ListAnswerCommentsUseCase) {}
+
+  @Get()
+  async handle(
+    @Query("page", queryValidationPipe) page: PageQueryParamSchema,
+    @Param("answerId") answerId: string
+  ) {
+    const result = await this.listAnswerComments.execute({ page, answerId });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    const answerComments = result.value.answerComments;
+
+    return { answerComments: answerComments.map(CommentPresenter.toHTTP) };
+  }
+}
