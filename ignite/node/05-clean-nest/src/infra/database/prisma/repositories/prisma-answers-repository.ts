e7516bdc240ee6e8ac1defe12f8d751b8efc24e1
Fabricio <fabricioliquesley15@gmail.com>
2024-07-25 19:21:15 -0300

    feat: use the answer attachment repository in methods create and save in the prisma answer repository

--- ignite/node/05-clean-nest/src/infra/database/prisma/repositories/prisma-answers-repository.ts
+++ ignite/node/05-clean-nest/src/infra/database/prisma/repositories/prisma-answers-repository.ts
@@ -4,10 +4,15 @@ import { AnswersRepository } from "@/domain/forum/application/repositories/answe
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 import { PrismaService } from "../prisma.service";
 import { PrismaAnswerMapper } from "../mappers/prisma-answer-mapper";
+import { AnswerAttachmentsRepository } from "@/domain/forum/application/repositories/answer-attachments-repository";
 
 @Injectable()
 export class PrismaAnswersRepository implements AnswersRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private answerAttachmentRepository: AnswerAttachmentsRepository
+  ) {}
+
   async findById(id: string): Promise<Answer | null> {
     const answer = await this.prisma.answer.findUnique({
       where: {
@@ -44,6 +49,10 @@ export class PrismaAnswersRepository implements AnswersRepository {
     await this.prisma.answer.create({
       data,
     });
+
+    await this.answerAttachmentRepository.createMany(
+      answer.attachments.getItems()
+    );
   }
 
   async delete(answer: Answer): Promise<void> {
@@ -57,11 +66,19 @@ export class PrismaAnswersRepository implements AnswersRepository {
   async save(answer: Answer): Promise<void> {
     const data = PrismaAnswerMapper.toPrisma(answer);
 
-    await this.prisma.answer.update({
-      where: {
-        id: data.id,
-      },
-      data,
-    });
+    await Promise.all([
+      this.prisma.answer.update({
+        where: {
+          id: data.id,
+        },
+        data,
+      }),
+      this.answerAttachmentRepository.createMany(
+        answer.attachments.getNewItems()
+      ),
+      this.answerAttachmentRepository.deleteMany(
+        answer.attachments.getRemovedItems()
+      ),
+    ]);
   }
 }
