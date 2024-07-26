02055210889ed7c1d17c061418ac12de0f51133b
Fabricio <fabricioliquesley15@gmail.com>
2024-07-25 21:39:15 -0300

    feat: add method findManyByQuestionIdWithAuthor in question comments repository contract

--- ignite/node/05-clean-nest/src/domain/forum/application/repositories/question-comments-repository.ts
+++ ignite/node/05-clean-nest/src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,9 +1,17 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionComment } from "../../enterprise/entities/question-comment";
+import { CommentWithAuthor } from "../../enterprise/entities/value-objects/comment-with-author";
 
 export abstract class QuestionCommentsRepository {
   abstract findById(id: string): Promise<QuestionComment | null>;
-  abstract findManyByQuestionId(id: string, params: PaginationParams): Promise<QuestionComment[]>;
+  abstract findManyByQuestionId(
+    id: string,
+    params: PaginationParams
+  ): Promise<QuestionComment[]>;
+  abstract findManyByQuestionIdWithAuthor(
+    id: string,
+    params: PaginationParams
+  ): Promise<CommentWithAuthor[]>;
   abstract create(questionComment: QuestionComment): Promise<void>;
   abstract delete(questionComment: QuestionComment): Promise<void>;
 }
