84da78f2f4d2b89150ffa00944be6a675e884ea1
Fabricio <fabricioliquesley15@gmail.com>
2024-07-26 18:38:12 -0300

    feat: implement the findManyByAnswerIdWithAuthor method in the in-memory answer comment repository

--- ignite/node/05-clean-nest/test/repositories/in-memory-answer-comments-repository.ts
+++ ignite/node/05-clean-nest/test/repositories/in-memory-answer-comments-repository.ts
@@ -2,10 +2,14 @@ import { DomainEvents } from "@/core/events/domain-events";
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-objects/comment-with-author";
+import { InMemoryStudentsRepository } from "./in-memory-student-repository";
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
+  constructor(private studentRepository: InMemoryStudentsRepository) {}
+
   public items: AnswerComment[] = [];
 
   async findById(answerCommentId: string) {
@@ -24,6 +28,34 @@ export class InMemoryAnswerCommentsRepository
       .slice((page - 1) * 20, page * 20);
 
     return answerComments;
+  }
+
+  async findManyByAnswerIdWithAuthor(id: string, { page }: PaginationParams) {
+    const answerComment = await this.items
+      .filter((item) => item.answerId.toString() === id)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentRepository.items.find((student) => {
+          return student.id === comment.authorId;
+        });
+
+        if (!author) {
+          throw new Error(
+            `Author with id ${comment.authorId.toString()} not found!`
+          );
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          authorId: comment.authorId,
+          authorName: author.name,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+        });
+      });
+
+    return answerComment;
   }
 
   async create(answerComment: AnswerComment) {
