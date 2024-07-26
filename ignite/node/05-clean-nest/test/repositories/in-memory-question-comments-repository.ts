2197ae64b5ef155608ff3254dcba762bb32fa3e8
Fabricio <fabricioliquesley15@gmail.com>
2024-07-25 21:41:36 -0300

    feat: implements the findManyByQuestionIdWithAuthor method in an in-memory comment repository

--- ignite/node/05-clean-nest/test/repositories/in-memory-question-comments-repository.ts
+++ ignite/node/05-clean-nest/test/repositories/in-memory-question-comments-repository.ts
@@ -2,11 +2,15 @@ import { DomainEvents } from "@/core/events/domain-events";
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
+import { InMemoryStudentsRepository } from "./in-memory-student-repository";
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-objects/comment-with-author";
 
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
   public items: QuestionComment[] = [];
+
+  constructor(private studentRepository: InMemoryStudentsRepository) {}
 
   async findById(questionCommentId: string) {
     const questionComment = this.items.find(
@@ -22,6 +26,34 @@ export class InMemoryQuestionCommentsRepository
     const questionComment = await this.items
       .filter((item) => item.questionId.toString() === id)
       .slice((page - 1) * 20, page * 20);
+
+    return questionComment;
+  }
+
+  async findManyByQuestionIdWithAuthor(id: string, { page }: PaginationParams) {
+    const questionComment = await this.items
+      .filter((item) => item.questionId.toString() === id)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentRepository.items.find((student) => {
+          return student.id === comment.authorId;
+        });
+
+        if (!author) {
+          throw new Error(
+            `Author with id ${comment.authorId.toString()} not found!`
+          );
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          authorId: comment.authorId,
+          authorName: author.name,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+        });
+      });
 
     return questionComment;
   }
