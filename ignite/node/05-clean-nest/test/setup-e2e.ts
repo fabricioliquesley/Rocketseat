95163ef5953ccaf8a9078feffece87a1562883a3
Fabricio <fabricioliquesley15@gmail.com>
2024-07-27 20:36:34 -0300

    chore: add redis settings to the e2e setup

--- ignite/node/05-clean-nest/test/setup-e2e.ts
+++ ignite/node/05-clean-nest/test/setup-e2e.ts
@@ -5,18 +5,27 @@ import { randomUUID } from "node:crypto";
 import "dotenv/config";
 import { execSync } from "node:child_process";
 import { DomainEvents } from "@/core/events/domain-events";
+import { Redis } from "ioredis";
+import { envSchema } from "@/infra/env/env";
 
 config({ path: ".env", override: true });
 config({ path: ".env.development.local", override: true });
 
+const env = envSchema.parse(process.env);
+
 const prisma = new PrismaClient();
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+});
 
 function generateUniqueDataBaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error("Please provider a DATABASE_URL environment variable.");
   }
 
-  const url = new URL(process.env.DATABASE_URL);
+  const url = new URL(env.DATABASE_URL);
 
   url.searchParams.set("schema", schemaId);
 
@@ -28,9 +37,11 @@ const schemaId = randomUUID();
 beforeAll(async () => {
   const databaseURL = generateUniqueDataBaseURL(schemaId);
 
-  process.env.DATABASE_URL = databaseURL;
+  env.DATABASE_URL = databaseURL;
 
   DomainEvents.shouldRun = false;
+
+  await redis.flushdb();
 
   execSync("npx prisma migrate deploy");
 });
