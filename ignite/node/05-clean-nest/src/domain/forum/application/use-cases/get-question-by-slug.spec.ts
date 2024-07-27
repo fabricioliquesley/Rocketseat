4259067143a7e48b1f172309d0bd8b92e3c0ca47
Fabricio <fabricioliquesley15@gmail.com>
2024-07-26 21:48:56 -0300

    chore: adapt the get question by slug test to check that the author and attachments are included with the question

--- ignite/node/05-clean-nest/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ ignite/node/05-clean-nest/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -4,8 +4,15 @@ import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
 import { makeQuestion } from "test/factories/make-question";
 import { Slug } from "../../enterprise/entities/value-objects/slug";
 import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { InMemoryAttachmentRepository } from "test/repositories/in-memory-attachment-repository";
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-student-repository";
+import { makeStudent } from "test/factories/make-student";
+import { makeAttachment } from "test/factories/make-attachment";
+import { makeQuestionAttachment } from "test/factories/make-question-attachment";
 
 let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
+let inMemoryAttachmentRepository: InMemoryAttachmentRepository;
+let inMemoryStudentRepository: InMemoryStudentsRepository;
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: GetQuestionBySlugUseCase;
 
@@ -13,15 +20,48 @@ describe("Get Question By Slug", () => {
   beforeEach(() => {
     inMemoryQuestionAttachmentsRepository =
       new InMemoryQuestionAttachmentsRepository();
+    inMemoryAttachmentRepository = new InMemoryAttachmentRepository();
+    inMemoryStudentRepository = new InMemoryStudentsRepository();
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
-      inMemoryQuestionAttachmentsRepository
+      inMemoryQuestionAttachmentsRepository,
+      inMemoryAttachmentRepository,
+      inMemoryStudentRepository
     );
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
   });
 
   it("should be able to find a question by slug", async () => {
-    const fakeQuestion = makeQuestion({ slug: Slug.create("question-slug") });
+    const student = makeStudent({ name: "John Doe" });
+
+    await inMemoryStudentRepository.create(student);
+
+    const attachment1 = makeAttachment();
+    const attachment2 = makeAttachment();
+
+    await inMemoryAttachmentRepository.create(attachment1);
+    await inMemoryAttachmentRepository.create(attachment2);
+
+    const fakeQuestion = makeQuestion({
+      slug: Slug.create("question-slug"),
+      authorId: student.id,
+    });
+
     await inMemoryQuestionsRepository.create(fakeQuestion);
+
+    const questionAttachment1 = makeQuestionAttachment({
+      attachmentId: attachment1.id,
+      questionId: fakeQuestion.id,
+    });
+
+    const questionAttachment2 = makeQuestionAttachment({
+      attachmentId: attachment2.id,
+      questionId: fakeQuestion.id,
+    });
+
+    inMemoryQuestionAttachmentsRepository.createMany([
+      questionAttachment1,
+      questionAttachment2,
+    ]);
 
     const result = await sut.execute({
       slug: "question-slug",
@@ -29,13 +69,28 @@ describe("Get Question By Slug", () => {
 
     expect(result.value).toMatchObject({
       question: expect.objectContaining({
-        id: fakeQuestion.id,
+        questionId: fakeQuestion.id,
+        title: fakeQuestion.title,
       }),
     });
     expect(result.value).toMatchObject({
       question: expect.objectContaining({
-        title: fakeQuestion.title
-      })
-    })
+        author: expect.objectContaining({
+          name: "John Doe",
+        }),
+      }),
+    });
+    expect(result.value).toMatchObject({
+      question: expect.objectContaining({
+        attachments: expect.arrayContaining([
+          expect.objectContaining({
+            url: attachment1.url,
+          }),
+          expect.objectContaining({
+            url: attachment2.url,
+          }),
+        ]),
+      }),
+    });
   });
 });
