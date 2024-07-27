08d54b0cee44172da6271e781b1380572ce111e4
Fabricio <fabricioliquesley15@gmail.com>
2024-07-26 21:56:52 -0300

    chore: adapt the get question by slug controller test to check that the author and attachments are sent with the question

--- ignite/node/05-clean-nest/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ ignite/node/05-clean-nest/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -4,24 +4,35 @@ import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
 import request from "supertest";
+import { AttachmentFactory } from "test/factories/make-attachment";
 import { QuestionFactory } from "test/factories/make-question";
+import { QuestionAttachmentFactory } from "test/factories/make-question-attachment";
 import { StudentFactory } from "test/factories/make-student";
 
 describe("Get question by slug (E2E)", () => {
   let app: INestApplication;
   let studentFactory: StudentFactory;
   let questionFactory: QuestionFactory;
+  let attachmentFactory: AttachmentFactory;
+  let questionAttachmentFactory: QuestionAttachmentFactory;
   let jwt: JwtService;
 
   beforeAll(async () => {
     const noduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
     }).compile();
 
     app = noduleRef.createNestApplication();
     studentFactory = noduleRef.get(StudentFactory);
     questionFactory = noduleRef.get(QuestionFactory);
+    attachmentFactory = noduleRef.get(AttachmentFactory);
+    questionAttachmentFactory = noduleRef.get(QuestionAttachmentFactory);
     jwt = noduleRef.get(JwtService);
 
     await app.init();
@@ -30,9 +41,18 @@ describe("Get question by slug (E2E)", () => {
   test("[GET] /questions/:", async () => {
     const user = await studentFactory.makePrismaStudent();
 
-    await questionFactory.makePrismaQuestion({
+    const question = await questionFactory.makePrismaQuestion({
       title: "My question",
       authorId: user.id,
+    });
+
+    const attachment = await attachmentFactory.makePrismaAttachment({
+      title: "Some attachment",
+    });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      questionId: question.id,
+      attachmentId: attachment.id,
     });
 
     const accessToken = jwt.sign({ sub: user.id.toString() });
@@ -43,7 +63,15 @@ describe("Get question by slug (E2E)", () => {
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({
-      question: expect.objectContaining({ title: "My question" }),
+      question: expect.objectContaining({
+        title: "My question",
+        author: expect.objectContaining({ name: user.name }),
+        attachments: expect.arrayContaining([
+          expect.objectContaining({
+            title: "Some attachment",
+          })
+        ])
+      }),
     });
   });
 });
