604f742b837c50ba31ebaed40a59ae624c489c3f
Fabricio <fabricioliquesley15@gmail.com>
2024-07-27 11:09:26 -0300

    feat: create the prisma question notification repository

---
+++ ignite/node/05-clean-nest/src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@nestjs/common";
+import { Notification } from "@/domain/notification/enterprise/entities/notification";
+import { PrismaService } from "../prisma.service";
+import { PrismaNotificationMapper } from "../mappers/prisma-notification-mapper";
+import { NotificationsRepository } from "@/domain/notification/application/repositories/notifications-repository";
+
+@Injectable()
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = await this.prisma.notification.findUnique({
+      where: {
+        id: notificationId,
+      },
+    });
+
+    if (!notification) return null;
+
+    return PrismaNotificationMapper.toDomain(notification);
+  }
+
+  async create(notification: Notification): Promise<void> {
+    const data = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prisma.notification.create({
+      data,
+    });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const data = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prisma.notification.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    });
+  }
+}
