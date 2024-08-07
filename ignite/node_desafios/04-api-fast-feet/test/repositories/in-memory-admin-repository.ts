706a4074352601ee768dac94556482bb5a2f069f
Fabricio <fabricioliquesley15@gmail.com>
2024-08-06 21:24:47 -0300

    feat: create an in-memory administration repository

---
+++ ignite/node_desafios/04-api-fast-feet/test/repositories/in-memory-admin-repository.ts
@@ -0,0 +1,18 @@
+import { AdminRepository } from "@/domain/carrier/application/repositories/admin-repository";
+import { Admin } from "@/domain/carrier/enterprise/entities/admin";
+
+export class InMemoryAdminRepository implements AdminRepository {
+  public items: Admin[] = [];
+
+  async create(admin: Admin): Promise<void> {
+    this.items.push(admin);
+  }
+
+  async findByCPF(cpf: string): Promise<Admin> {
+    const admin = this.items.find((admin) => admin.cpf === cpf);
+
+    if (!admin) return null;
+
+    return admin;
+  }
+}
