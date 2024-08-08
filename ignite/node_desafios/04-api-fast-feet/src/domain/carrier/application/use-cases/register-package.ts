b5e483b4f429012b61707199df3701d60cc30e06
Fabricio <fabricioliquesley15@gmail.com>
2024-08-08 15:09:18 -0300

    feat: create the use case to register a package

---
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/use-cases/register-package.ts
@@ -0,0 +1,50 @@
+import { Either, left, right } from "@/core/either";
+import { UniquesEntityId } from "@/core/entities/unique-entity-id";
+import { RecipientRepository } from "../repositories/recipient-repository";
+import { Package } from "../../enterprise/entities/package";
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error";
+import { PackageRepository } from "../repositories/package-repository";
+
+export interface RegisterPackageUseCaseRequest {
+  code: string;
+  type: string;
+  recipientId: UniquesEntityId;
+}
+
+type RegisterPackageUseCaseResponse = Either<
+  ResourceNotFoundError,
+  { package: Package }
+>;
+
+export class RegisterPackageUseCase {
+  constructor(
+    private recipientRepository: RecipientRepository,
+    private packageRepository: PackageRepository
+  ) {}
+
+  async execute({
+    code,
+    type,
+    recipientId,
+  }: RegisterPackageUseCaseRequest): Promise<RegisterPackageUseCaseResponse> {
+    const recipient = await this.recipientRepository.findById(
+      recipientId.toString()
+    );
+
+    if (!recipient) {
+      return left(new ResourceNotFoundError());
+    }
+
+    const _package = Package.create({
+      code,
+      type,
+      recipientId,
+    });
+
+    await this.packageRepository.create(_package);
+
+    return right({
+      package: _package,
+    });
+  }
+}
