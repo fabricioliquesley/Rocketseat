453535fe2e3fa689d6df1edc18628f3412a006ca
Fabricio <fabricioliquesley15@gmail.com>
2024-08-08 14:57:13 -0300

    feat: add the method findById in admin repository contract

--- ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/admin-repository.ts
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/admin-repository.ts
@@ -2,5 +2,6 @@ import { Admin } from "../../enterprise/entities/admin";
 
 export abstract class AdminRepository {
   abstract findByCPF(cpf: string): Promise<Admin | null>;
+  abstract findById(id: string): Promise<Admin | null>;
   abstract create(admin: Admin): Promise<void>;
 }
