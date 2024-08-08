9bccbed2a26815ea66e2e1fc596d82910ff290f4
Fabricio <fabricioliquesley15@gmail.com>
2024-08-08 14:59:11 -0300

    feat: add the method findById in delivery man repository contract

--- ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/delivery-man-repository.ts
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/delivery-man-repository.ts
@@ -2,6 +2,7 @@ import { DeliveryMan } from "../../enterprise/entities/delivery-man";
 
 export abstract class DeliveryManRepository {
   abstract findByCPF(cpf: string): Promise<DeliveryMan | null>;
+  abstract findById(id: string): Promise<DeliveryMan | null>;
   abstract create(deliveryMan: DeliveryMan): Promise<void>;
   abstract delete(deliveryMan: DeliveryMan): Promise<void>;
   abstract save(deliveryMan: DeliveryMan): Promise<void>;
