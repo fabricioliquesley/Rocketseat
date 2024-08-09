901022caab9ec1130a1d5fd4f89b389d2cf7f5ca
Fabricio <fabricioliquesley15@gmail.com>
2024-08-09 20:05:49 -0300

    feat: add Toaster and Helmet in App.tsx

--- ignite/react/04-pizza-shop/src/App.tsx
+++ ignite/react/04-pizza-shop/src/App.tsx
@@ -1,11 +1,15 @@
+import "./global.css";
+
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
 
 export const App = () => {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop"/>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors/>
       <RouterProvider router={router} />
     </HelmetProvider>
   );
