f681111f36eebab3e5938a33b265dbfa29c5c76c
Fabricio <fabricioliquesley15@gmail.com>
2024-08-10 10:55:56 -0300

    feat: create a use case to update recipient

---
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/use-cases/update-recipient.ts
@@ -0,0 +1,62 @@
+import { Either, left, right } from "@/core/either";
+import { RecipientRepository } from "../repositories/recipient-repository";
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error";
+import { AdminRepository } from "../repositories/admin-repository";
+import { UserIsNotAdminError } from "./errors/user-is-not-admin-error";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+
+export interface UpdateRecipientUseCaseRequest {
+  name?: string;
+  cpf?: string;
+  recipientCPF: string;
+  adminCPF: string;
+}
+
+type UpdateRecipientUseCaseResponse = Either<
+  ResourceNotFoundError | UserIsNotAdminError | UserAlreadyExistsError,
+  {}
+>;
+
+export class UpdateRecipientUseCase {
+  constructor(
+    private recipientRepository: RecipientRepository,
+    private adminRepository: AdminRepository
+  ) {}
+
+  async execute({
+    name,
+    cpf,
+    recipientCPF,
+    adminCPF,
+  }: UpdateRecipientUseCaseRequest): Promise<UpdateRecipientUseCaseResponse> {
+    const recipient = await this.recipientRepository.findByCPF(recipientCPF);
+
+    if (!recipient) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (cpf) {
+      const recipientWithSameCpf = await this.recipientRepository.findByCPF(
+        cpf
+      );
+      const adminWithSameCpf = await this.adminRepository.findByCPF(cpf);
+
+      if (recipientWithSameCpf || adminWithSameCpf) {
+        return left(new UserAlreadyExistsError(cpf));
+      }
+    }
+
+    const userAdmin = await this.adminRepository.findByCPF(adminCPF);
+
+    if (userAdmin.role !== "admin") {
+      return left(new UserIsNotAdminError(adminCPF));
+    }
+
+    recipient.name = name ?? recipient.name;
+    recipient.cpf = cpf ?? recipient.cpf;
+
+    await this.recipientRepository.save(recipient);
+
+    return right({});
+  }
+}
