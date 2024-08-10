1d16505406b947ce3b04aa191a54c3568a019841
Fabricio <fabricioliquesley15@gmail.com>
2024-08-09 21:51:50 -0300

    chore: use theme provider in App.tsx

--- ignite/react/04-pizza-shop/src/App.tsx
+++ ignite/react/04-pizza-shop/src/App.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export const App = () => {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors/>
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 };
