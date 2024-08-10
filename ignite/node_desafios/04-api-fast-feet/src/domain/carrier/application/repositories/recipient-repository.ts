862a9489942ff26273a282718be8ae1c6dfeb7c9
Fabricio <fabricioliquesley15@gmail.com>
2024-08-10 10:43:29 -0300

    feat: add new methods to the recipient repository contract

--- ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/recipient-repository.ts
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/recipient-repository.ts
@@ -1,3 +1,9 @@
+import { Recipient } from "../../enterprise/entities/recipient";
+
 export abstract class RecipientRepository {
-  abstract findById(id: string): Promise<any | null>;
+  abstract findById(id: string): Promise<Recipient | null>;
+  abstract findByCPF(cpf: string): Promise<Recipient | null>;
+  abstract create(recipient: Recipient): Promise<void>;
+  abstract delete(recipient: Recipient): Promise<void>;
+  abstract save(recipient: Recipient): Promise<void>;
 }
