9e433c7f08c24d19e11358e51737f4823e0827b0
Fabricio <fabricioliquesley15@gmail.com>
2024-08-10 13:52:34 -0300

    feat: create the order details component

---
+++ ignite/react/04-pizza-shop/src/pages/app/orders/order-details.tsx
@@ -0,0 +1,101 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export interface OrderDetailsProps {}
+
+export const OrderDetails = ({}: OrderDetailsProps) => {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Pedido: Px00001</DialogTitle>
+        <DialogDescription>Detalhes do pedido.</DialogDescription>
+      </DialogHeader>
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-full bg-slate-400" />
+                  <span className="font-medium text-muted-foreground">
+                    Pendente
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Cliente</TableCell>
+              <TableCell className="flex justify-end">
+                Fabricio Liquesley dos Santos
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Telefone</TableCell>
+              <TableCell className="flex justify-end">
+                (11) 97070-7070
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">E-mail</TableCell>
+              <TableCell className="flex justify-end">
+                fabricio.test@email.com
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">
+                Realizado há
+              </TableCell>
+              <TableCell className="flex justify-end">há 3 minutos</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead className="text-right">Qtd</TableHead>
+              <TableHead className="text-right">Preço</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Pizza Pepperoni Broto</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 33,50</TableCell>
+              <TableCell className="text-right">R$ 67,00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Pizza Mussarela Media</TableCell>
+              <TableCell className="text-right">1</TableCell>
+              <TableCell className="text-right">R$ 45,00</TableCell>
+              <TableCell className="text-right">R$ 45,00</TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total do pedido</TableCell>
+              <TableCell className="text-right font-medium">
+                R$ 112,00
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </DialogContent>
+  );
+};
