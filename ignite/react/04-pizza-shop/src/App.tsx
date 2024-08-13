5670c3e5a4efc624ae5d7672baaa54c325d7c7b6
Fabricio <fabricioliquesley15@gmail.com>
2024-08-13 20:38:49 -0300

    feat: use QueryClientProvider in app.tsx

--- ignite/react/04-pizza-shop/src/App.tsx
+++ ignite/react/04-pizza-shop/src/App.tsx
@@ -5,6 +5,8 @@ import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClint } from "./lib/react-query";
 
 export const App = () => {
   return (
@@ -12,7 +14,9 @@ export const App = () => {
       <ThemeProvider storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClint}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
