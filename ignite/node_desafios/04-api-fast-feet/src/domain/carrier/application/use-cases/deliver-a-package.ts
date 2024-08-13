934bf8aae364e00c8b7bfad3d574ce2418b0bcd5
Fabricio <fabricioliquesley15@gmail.com>
2024-08-13 19:30:39 -0300

    feat: create a use case to deliver a package

---
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/use-cases/deliver-a-package.ts
@@ -0,0 +1,54 @@
+import { Either, left, right } from "@/core/either";
+import { UniquesEntityId } from "@/core/entities/unique-entity-id";
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error";
+import { PackageRepository } from "../repositories/package-repository";
+import { DeliveryManRepository } from "../repositories/delivery-man-repository";
+import { DeliveryManNotMatchError } from "./errors/delivery-man-not-match-error";
+
+export interface DeliverAPackageUseCaseRequest {
+  packageId: UniquesEntityId;
+  deliveryManId: UniquesEntityId;
+}
+
+type DeliverAPackageUseCaseResponse = Either<
+  ResourceNotFoundError | DeliveryManNotMatchError,
+  {}
+>;
+
+export class DeliverAPackageUseCase {
+  constructor(
+    private packageRepository: PackageRepository,
+    private deliveryManRepository: DeliveryManRepository
+  ) {}
+
+  async execute({
+    packageId,
+    deliveryManId,
+  }: DeliverAPackageUseCaseRequest): Promise<DeliverAPackageUseCaseResponse> {
+    const _package = await this.packageRepository.findById(
+      packageId.toString()
+    );
+
+    if (!_package) {
+      return left(new ResourceNotFoundError());
+    }
+
+    const deliveryMan = await this.deliveryManRepository.findById(
+      deliveryManId.toString()
+    );
+
+    if (!deliveryMan) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (_package.deliveryManId !== deliveryManId) {
+      return left(new DeliveryManNotMatchError());
+    }
+
+    _package.changeSituation("delivered");
+
+    await this.packageRepository.save(_package);
+
+    return right({});
+  }
+}
