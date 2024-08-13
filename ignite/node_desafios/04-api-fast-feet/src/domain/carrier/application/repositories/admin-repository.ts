77ab5701075bbd9acba89a5f47d9e9a4168abd62
Fabricio <fabricioliquesley15@gmail.com>
2024-08-12 22:12:53 -0300

    feat: add the method save in admin repository contract

--- ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/admin-repository.ts
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/admin-repository.ts
@@ -4,4 +4,5 @@ export abstract class AdminRepository {
   abstract findByCPF(cpf: string): Promise<Admin | null>;
   abstract findById(id: string): Promise<Admin | null>;
   abstract create(admin: Admin): Promise<void>;
+  abstract save(admin: Admin): Promise<void>;
 }
