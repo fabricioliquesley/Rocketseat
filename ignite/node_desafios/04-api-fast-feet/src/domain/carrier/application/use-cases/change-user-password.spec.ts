da7f02dd293b9154fb49f4b3a377b4aad0cca807
Fabricio <fabricioliquesley15@gmail.com>
2024-08-13 19:35:18 -0300

    feat: adapt the test of the use case of changing the user's password to work with admin

--- ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/use-cases/change-user-password.spec.ts
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/use-cases/change-user-password.spec.ts
@@ -2,24 +2,33 @@ import { InMemoryDeliveryManRepository } from "test/repositories/in-memory-deliv
 import { ChangeUserPasswordUseCase } from "./change-user-password";
 import { makeDeliveryMan } from "test/factories/make-delivery-man";
 import { PasswordNotMatchError } from "./errors/password-not-match-error";
+import { InMemoryAdminRepository } from "test/repositories/in-memory-admin-repository";
+import { makeAdmin } from "test/factories/make-admin";
 
 let inMemoryDeliveryManRepository: InMemoryDeliveryManRepository;
+let inMemoryAdminRepository: InMemoryAdminRepository;
 let sut: ChangeUserPasswordUseCase;
 
 beforeEach(() => {
   inMemoryDeliveryManRepository = new InMemoryDeliveryManRepository();
-  sut = new ChangeUserPasswordUseCase(inMemoryDeliveryManRepository);
+  inMemoryAdminRepository = new InMemoryAdminRepository();
+  sut = new ChangeUserPasswordUseCase(
+    inMemoryDeliveryManRepository,
+    inMemoryAdminRepository
+  );
 });
 
 describe("Change user password", () => {
   it("Should be able to change the user password", async () => {
-    const user = makeDeliveryMan({
-      password: "123456",
-    });
+    const user = makeDeliveryMan({ password: "123456" });
+    const admin = makeAdmin();
+
     inMemoryDeliveryManRepository.create(user);
+    inMemoryAdminRepository.create(admin);
 
     const result = await sut.execute({
       userId: user.id,
+      adminId: admin.id,
       currentPassword: "123456",
       newPassword: "654321",
     });
@@ -34,10 +43,14 @@ describe("Change user password", () => {
 
   it("Should not be possible to change the user's password if the current password is wrong", async () => {
     const user = makeDeliveryMan();
+    const admin = makeAdmin();
+
     inMemoryDeliveryManRepository.create(user);
+    inMemoryAdminRepository.create(admin);
 
     const result = await sut.execute({
       userId: user.id,
+      adminId: admin.id,
       currentPassword: "123456",
       newPassword: "654321",
     });
