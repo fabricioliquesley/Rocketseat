47b68231fc5625d7af9e1d876eadb5fac0b6ae65
Fabricio <fabricioliquesley15@gmail.com>
2024-08-15 18:38:51 -0300

    feat: use the function to obtain the monthly revenue in the monthly revenue card component

--- ignite/react/04-pizza-shop/src/pages/app/dashboard/month-revenue-card.tsx
+++ ignite/react/04-pizza-shop/src/pages/app/dashboard/month-revenue-card.tsx
@@ -1,7 +1,15 @@
+import { getMonthRevenue } from "@/api/get-month-revenue";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "@tanstack/react-query";
 import { DollarSign } from "lucide-react";
 
 export const MonthRevenueCard = () => {
+  const { data: monthMetrics } = useQuery({
+    queryKey: ["metrics", "month-revenue-amount"],
+    queryFn: getMonthRevenue,
+  });
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,11 +19,28 @@ export const MonthRevenueCard = () => {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">R$ 2148,60</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">+2%</span> em
-          relação ao mês passado
-        </p>
+        {monthMetrics && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {(monthMetrics.receipt / 100).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthMetrics.diffFromLastMonth >= 0 ? (
+                <span className="text-emerald-500 dark:text-emerald-400">
+                  +{monthMetrics.diffFromLastMonth}%
+                </span>
+              ) : (
+                <span className="text-rose-500 dark:text-rose-400">
+                  {monthMetrics.diffFromLastMonth}%
+                </span>
+              )}{" "}
+              em relação ao mês passado
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   );
