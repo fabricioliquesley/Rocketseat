a347c1c7aa6b6235f87f1de4bbb8970e76d860f3
Fabricio <fabricioliquesley15@gmail.com>
2024-08-14 22:05:15 -0300

    feat: add methods findManyByDeliveryManId and findManyNearby in the package repository contract

--- ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/package-repository.ts
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/repositories/package-repository.ts
@@ -1,7 +1,22 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Package } from "../../enterprise/entities/package";
+
+export type FindManyNearbyParams = {
+  deliveryManId: string;
+  latitude: number;
+  longitude: number;
+};
 
 export abstract class PackageRepository {
   abstract findById(id: string): Promise<null | Package>;
+  abstract findManyByDeliveryManId(
+    deliveryManId: string,
+    params: PaginationParams
+  ): Promise<Package[]>;
+  abstract findManyNearby(
+    params: FindManyNearbyParams,
+    paginationParams: PaginationParams
+  ): Promise<Package[] | null>;
   abstract create(_package: Package): Promise<void>;
   abstract delete(_package: Package): Promise<void>;
   abstract save(_package: Package): Promise<void>;
