a711fab70d46b32bf6ceabbe43a3c52b4e5207e3
Fabricio <fabricioliquesley15@gmail.com>
2024-08-14 22:11:24 -0300

    feat: implements the methods findManyByDeliveryManId and findManyNearby in in-memory package repository

--- ignite/node_desafios/04-api-fast-feet/test/repositories/in-memory-package-repository.ts
+++ ignite/node_desafios/04-api-fast-feet/test/repositories/in-memory-package-repository.ts
@@ -1,5 +1,10 @@
-import { PackageRepository } from "@/domain/carrier/application/repositories/package-repository";
+import { PaginationParams } from "@/core/repositories/pagination-params";
+import {
+  FindManyNearbyParams,
+  PackageRepository,
+} from "@/domain/carrier/application/repositories/package-repository";
 import { Package } from "@/domain/carrier/enterprise/entities/package";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
 export class InMemoryPackageRepository implements PackageRepository {
   public items: Package[] = [];
@@ -8,6 +13,42 @@ export class InMemoryPackageRepository implements PackageRepository {
     const _package = this.items.find((item) => item.id.toString() === id);
 
     return _package;
+  }
+
+  async findManyByDeliveryManId(
+    deliveryManId: string,
+    { page }: PaginationParams
+  ): Promise<Package[]> {
+    const packages = this.items
+      .filter((item) => {
+        return (
+          item.deliveryManId.toString() === deliveryManId &&
+          item.situation === "delivered"
+        );
+      })
+      .slice((page - 1) * 20, page * 20);
+
+    return packages;
+  }
+
+  async findManyNearby(
+    { deliveryManId, latitude, longitude }: FindManyNearbyParams,
+    { page }: PaginationParams
+  ): Promise<Package[]> {
+    const packages = this.items
+      .filter((item) => {
+        const distance = getDistanceBetweenCoordinates(
+          { latitude, longitude },
+          { latitude: Number(item.latitude), longitude: Number(item.longitude) }
+        );
+
+        return distance < 20 && item.deliveryManId.toString() === deliveryManId;
+      })
+      .slice((page - 1) * 20, page * 20);
+
+    if (!packages) return null;
+
+    return packages;
   }
 
   async create(_package: Package): Promise<void> {
