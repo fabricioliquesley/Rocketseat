5c0b94c69bc681d622d3d86a1fbdb79d2c20a22c
Fabricio <fabricioliquesley15@gmail.com>
2024-08-16 20:27:46 -0300

    feat: create a use case to register a delivery man

---
+++ ignite/node_desafios/04-api-fast-feet/src/domain/carrier/application/use-cases/register-delivery-man.ts
@@ -0,0 +1,48 @@
+import { Either, left, right } from "src/core/either";
+import { DeliveryMan } from "../../enterprise/entities/delivery-man";
+import { DeliveryManRepository } from "../repositories/delivery-man-repository";
+import { HashGenerator } from "../cryptography/hashGenerator";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+
+interface RegisterDeliveryManUseCaseRequest {
+  name: string;
+  cpf: string;
+  password: string;
+}
+
+type RegisterDeliveryManUseCaseResponse = Either<
+  UserAlreadyExistsError,
+  { deliveryman: DeliveryMan }
+>;
+
+export class RegisterDeliveryManUseCase {
+  constructor(
+    private deliverymanRepositoryRepository: DeliveryManRepository,
+    private hashGenerator: HashGenerator
+  ) {}
+
+  async execute({
+    name,
+    cpf,
+    password,
+  }: RegisterDeliveryManUseCaseRequest): Promise<RegisterDeliveryManUseCaseResponse> {
+    const deliverymanWithSameCpf =
+      await this.deliverymanRepositoryRepository.findByCPF(cpf);
+
+    if (deliverymanWithSameCpf) {
+      return left(new UserAlreadyExistsError(cpf));
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password);
+
+    const deliveryman = DeliveryMan.create({
+      name,
+      cpf,
+      password: hashedPassword,
+    });
+
+    await this.deliverymanRepositoryRepository.create(deliveryman);
+
+    return right({ deliveryman });
+  }
+}
