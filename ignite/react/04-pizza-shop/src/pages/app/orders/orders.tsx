120df59c3230f659c1b27fe0fc6df44331e284d8
Fabricio <fabricioliquesley15@gmail.com>
2024-08-16 14:47:33 -0300

    feat: use the order table skeleton in order page

--- ignite/react/04-pizza-shop/src/pages/app/orders/orders.tsx
+++ ignite/react/04-pizza-shop/src/pages/app/orders/orders.tsx
@@ -13,6 +13,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getOrders } from "@/api/get-orders";
 import { useSearchParams } from "react-router-dom";
 import { z } from "zod";
+import { OrderTableSkeleton } from "./order-table-skeleton";
 
 export const Orders = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -26,7 +27,7 @@ export const Orders = () => {
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -67,6 +68,7 @@ export const Orders = () => {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
                 {result &&
                   result.orders.map((order) => {
                     return <OrderTableRow order={order} key={order.orderId} />;
