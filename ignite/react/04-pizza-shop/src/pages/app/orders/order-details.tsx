7083183358784c829c4fad6f0339efa736aa47ae
Fabricio <fabricioliquesley15@gmail.com>
2024-08-16 14:49:37 -0300

    feat: use the OrderDetailsSkeleton in the order details component

--- ignite/react/04-pizza-shop/src/pages/app/orders/order-details.tsx
+++ ignite/react/04-pizza-shop/src/pages/app/orders/order-details.tsx
@@ -19,6 +19,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { OrderDetailsSkeleton } from "./order-details-skeleton";
 
 export interface OrderDetailsProps {
   orderId: string;
@@ -38,7 +39,7 @@ export const OrderDetails = ({ orderId, open }: OrderDetailsProps) => {
         <DialogTitle>Pedido: {orderId}</DialogTitle>
         <DialogDescription>Detalhes do pedido.</DialogDescription>
       </DialogHeader>
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -130,6 +131,8 @@ export const OrderDetails = ({ orderId, open }: OrderDetailsProps) => {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   );
