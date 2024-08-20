76ab339c037e5a60a0674df2910d48adb4c21d62
Fabricio <fabricioliquesley15@gmail.com>
2024-08-20 20:10:10 -0300

    feat: create test end to end to the login logic

---
+++ ignite/react/04-pizza-shop/test/sign-in.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/test";
+
+test("sign in successfully", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByLabel("Seu e-mail").fill("john@example.com");
+  await page.getByRole("button", { name: "Acessar painel" }).click();
+
+  const toast = page.getByText(
+    "Enviamos um link de autenticação para seu e-mail.",
+  );
+
+  await expect(toast).toBeVisible();
+});
+
+test("sign in with wrong credentials", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByLabel("Seu e-mail").fill("wrong@example.com");
+  await page.getByRole("button", { name: "Acessar painel" }).click();
+
+  const toast = page.getByText("Credencias invalidas");
+
+  await expect(toast).toBeVisible();
+});
+
+test("navigate to new restaurant page", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByRole("link", { name: "Novo estabelecimento" }).click();
+
+  const pageUrl = page.url();
+
+  expect(pageUrl).toContain("/sign-up");
+});
